e51a278923b3a0c3298703d40c6c4b7258ca0c3c
Artem Yurchenko <44875844+murcake@users.noreply.github.com>
2021-10-15 15:49:59 +0300

    fix ExpressionSplitter: support multiple function calls as arguments (#69)

--- warp/yul/main.py
+++ warp/yul/main.py
@@ -1,9 +1,13 @@
+from __future__ import annotations
+
 import json
 import shutil
 import subprocess
 import sys
 
+import yul.yul_ast as ast
 from starkware.cairo.lang.compiler.parser import parse_file
+from yul.Artifacts import Artifacts
 from yul.ExpressionSplitter import ExpressionSplitter
 from yul.ForLoopEliminator import ForLoopEliminator
 from yul.ForLoopSimplifier import ForLoopSimplifier
@@ -16,7 +20,7 @@ from yul.ScopeFlattener import ScopeFlattener
 from yul.SwitchToIfVisitor import SwitchToIfVisitor
 from yul.ToCairoVisitor import ToCairoVisitor
 from yul.parse import parse_node
-from yul.utils import get_public_functions
+from yul.utils import get_public_functions, get_function_mutabilities
 
 AST_GENERATOR = "kudu"
 
@@ -24,10 +28,6 @@ AST_GENERATOR = "kudu"
 def generate_cairo(sol_src_path, main_contract):
     if not shutil.which(AST_GENERATOR):
         sys.exit(f"Please install {AST_GENERATOR} first")
-
-    with open(sol_src_path) as f:
-        sol_source = f.read()
-        public_functions = get_public_functions(sol_source)
 
     try:
         result = subprocess.run(
@@ -39,8 +39,30 @@ def generate_cairo(sol_src_path, main_contract):
         print(e.stderr.decode("utf-8"), file=sys.stderr)
         raise e
 
-    name_gen = NameGenerator()
+    with open(sol_src_path) as f:
+        sol_source = f.read()
+        public_functions = get_public_functions(sol_source)
+        function_mutabilities = get_function_mutabilities(sol_source)
+
+    artifacts_manager = Artifacts(sol_src_path)
     yul_ast = parse_node(json.loads(result.stdout))
+    return generate_from_yul(
+        yul_ast,
+        main_contract,
+        public_functions,
+        function_mutabilities,
+        artifacts_manager,
+    )
+
+
+def generate_from_yul(
+    yul_ast: ast.Node,
+    main_contract: str,
+    public_functions: list[str],
+    function_mutabilities: dict[str, str],
+    artifacts_manager: Artifacts,
+):
+    name_gen = NameGenerator()
     yul_ast = ForLoopSimplifier().map(yul_ast)
     yul_ast = ForLoopEliminator(name_gen).map(yul_ast)
     yul_ast = MangleNamesVisitor().map(yul_ast)
@@ -51,7 +73,14 @@ def generate_cairo(sol_src_path, main_contract):
     yul_ast = LeaveNormalizer().map(yul_ast)
     yul_ast = RevertNormalizer().map(yul_ast)
     yul_ast = FunctionPruner(public_functions).map(yul_ast)
-    cairo_visitor = ToCairoVisitor(sol_source, sol_src_path, main_contract, name_gen)
+
+    cairo_visitor = ToCairoVisitor(
+        main_contract,
+        public_functions,
+        function_mutabilities,
+        name_gen,
+        artifacts_manager,
+    )
     cairo_code = cairo_visitor.translate(yul_ast)
     return parse_file(cairo_code).format()
 
