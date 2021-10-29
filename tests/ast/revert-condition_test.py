768863cf41b52523a56c8f8eb92906b9cb13b554
Carmen I Cabrera <kr1000a@gmail.com>
2021-10-28 21:36:10 -0400

    add test for revert

---
+++ tests/ast/revert-condition_test.py
@@ -0,0 +1,37 @@
+import difflib
+import os
+import pytest
+
+from yul.AstTools import AstParser, AstPrinter
+from yul.ExpressionSplitter import ExpressionSplitter
+from yul.NameGenerator import NameGenerator
+from yul.RevertNormalizer import RevertNormalizer
+from yul.ScopeFlattener import ScopeFlattener
+
+def test_revert_condition():
+    ast_file_path = str(__file__)[:-8] + '.ast'
+    with open(ast_file_path, 'r') as ast_file:
+        parser = AstParser(ast_file.read())
+        
+    yul_ast = parser.parse_node()
+    name_gen = NameGenerator()
+    yul_ast = ExpressionSplitter(name_gen).map(yul_ast)
+    yul_ast = RevertNormalizer().map(yul_ast)
+    yul_ast = ScopeFlattener(name_gen).map(yul_ast)
+
+    generated_ast = AstPrinter().format(yul_ast)
+    temp_file_path = f"{ast_file_path}.temp"
+    with open(temp_file_path, 'w') as temp_file:
+        temp_file.write(generated_ast)
+
+    with open(ast_file_path + ".result", "r") as result_file:
+        expected_ast = result_file.read()
+
+    compare_codes(generated_ast.splitlines(), expected_ast.splitlines())
+    os.remove(temp_file_path)
+
+def compare_codes(lines1, lines2):
+    d = difflib.unified_diff(lines1, lines2, n=1, lineterm="")
+
+    message = "\n".join([line for line in d])
+    assert len(message) == 0, message
