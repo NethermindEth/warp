e0cbcea0b1dbc0ee6fe7ad0c74a32ac6e70ef841
BitBaseBit <vardygreg23@gmail.com>
2021-12-31 17:53:13 +0000

    update ingore_files.json & init_test.sh

--- tests/semantic/init_test.sh
+++ tests/semantic/init_test.sh
@@ -2,7 +2,7 @@
 shopt -s globstar
 
 function prepend_solidity_version() {
-  for solFile in **/*.sol;
+  for solFile in $(find . -name "*.sol" -type f);
   do
       echo "$solFile"
       printf '%s\n%s\n' "pragma solidity ^0.8.6;" "$(cat "$solFile")" > "$solFile"
@@ -14,7 +14,10 @@ then
   echo "Semantic Tests already initialized"
 else
   git clone git@github.com:ethereum/solidity.git
-
+  mv solidity tests/semantic
+  cd solidity
+  git checkout e5eed63a3e83d698d8657309fd371248945a1cda
+  cd ..
   pushd solidity/test/libsolidity/semanticTests || exit
   prepend_solidity_version
   popd || exit
