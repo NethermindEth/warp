9e06f644eefbdf074929679d1694d7d306cb3f2f
Atharva Nimbalkar <30515829+Glitch18@users.noreply.github.com>
2022-02-18 16:52:19 +0530

    Implement ShortCircuitToConditional pass (#59)

--- src/passes/builtinHandler/index.ts
+++ src/passes/builtinHandler/index.ts
@@ -4,12 +4,14 @@ import { MathsOperationToFunction } from './MathsOperationToFunction';
 import { ExplicitConversionToFunc } from './explicitConversionToFunc';
 import { MsgSender } from './msgSender';
 import { Require } from './require';
+import { ShortCircuitToConditional } from './shortCircuitToConditional';
 
 export class BuiltinHandler extends ASTMapper {
   map(ast: AST): AST {
     ast = new MsgSender().map(ast);
     ast = new Require().map(ast);
     ast = new ExplicitConversionToFunc().map(ast);
+    ast = new ShortCircuitToConditional().map(ast);
     ast = new MathsOperationToFunction().map(ast);
     return ast;
   }
---
+++ src/passes/builtinHandler/shortCircuitToConditional.ts
@@ -0,0 +1,38 @@
+import { BinaryOperation, Conditional } from 'solc-typed-ast';
+import { AST } from '../../ast/ast';
+import { ASTMapper } from '../../ast/mapper';
+import { createBoolLiteral } from '../../utils/utils';
+
+export class ShortCircuitToConditional extends ASTMapper {
+  visitBinaryOperation(node: BinaryOperation, ast: AST): void {
+    this.commonVisit(node, ast);
+
+    if (node.operator == '&&') {
+      const replacementExpression = new Conditional(
+        ast.reserveId(),
+        node.src,
+        'Conditional',
+        'bool',
+        node.vLeftExpression,
+        node.vRightExpression,
+        createBoolLiteral(false, ast),
+      );
+
+      ast.replaceNode(node, replacementExpression);
+    }
+
+    if (node.operator == '||') {
+      const replacementExpression = new Conditional(
+        ast.reserveId(),
+        node.src,
+        'Conditional',
+        'bool',
+        node.vLeftExpression,
+        createBoolLiteral(true, ast),
+        node.vRightExpression,
+      );
+
+      ast.replaceNode(node, replacementExpression);
+    }
+  }
+}
