f4913815ea0211dbe4f2a251742ab2e71441807e
DomHenderson <40724842+DomHenderson@users.noreply.github.com>
2022-03-30 14:19:47 +0100

    Add range check to wide equality (#279)
    * Add range check to wide equality
    
    * Fix testnet CI bug

--- src/warplib/implementations/maths/eq.ts
+++ src/warplib/implementations/maths/eq.ts
@@ -1,7 +1,12 @@
 import { BinaryOperation } from 'solc-typed-ast';
 import { AST } from '../../../ast/ast';
+import { Implicits } from '../../../utils/implicits';
 import { Comparison } from '../../utils';
 
 export function functionaliseEq(node: BinaryOperation, ast: AST): void {
-  Comparison(node, 'eq', 'only256', false, () => [], ast);
+  const implicitsFn = (wide: boolean): Implicits[] => {
+    if (wide) return ['range_check_ptr'];
+    return [];
+  };
+  Comparison(node, 'eq', 'only256', false, implicitsFn, ast);
 }
