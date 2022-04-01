477d8488ab1afcb1a8ca7d3cf73d8b235de69540
Purva-Chaudhari <purva.chaudhari02@gmail.com>
2022-04-01 12:40:04 +0530

    used visitFunctionCall

--- example_contracts/libraries/using_for_simple.sol
+++ example_contracts/libraries/using_for_simple.sol
@@ -2,14 +2,6 @@ pragma solidity ^0.8.6;
 
 // SPDX-License-Identifier: MIT
 
-library Nums {
-  function amIReal(int256 t, bool d) external returns (bool) {
-    return true;
-  }
-  function amIReal(uint256 t, bool d) external returns (bool) {
-    return false;
-  }
-}
 
 contract WARP {
 
@@ -19,5 +11,14 @@ contract WARP {
     uint256 t = 0;
     bool g = Nums.amIReal(t, d);
     return t.amIReal(d);
+  }
+}
+
+library Nums {
+  function amIReal(int256 t, bool d) external returns (bool) {
+    return true;
+  }
+  function amIReal(uint256 t, bool d) external returns (bool) {
+    return false;
   }
 }
--- src/passes/referencedLibraries.ts
+++ src/passes/referencedLibraries.ts
@@ -1,22 +1,34 @@
-import { ContractDefinition, ContractKind } from 'solc-typed-ast';
+import assert = require('assert');
+import { ContractDefinition, ContractKind, FunctionCall, MemberAccess } from 'solc-typed-ast';
 import { AST } from '../ast/ast';
 import { ASTMapper } from '../ast/mapper';
 
-const libraries: number[] = [];
 export class ReferencedLibraries extends ASTMapper {
-  visitContractDefinition(node: ContractDefinition, ast: AST): void {
-    if (node.kind === ContractKind.Library) {
-      libraries.push(node.id);
-    } else {
-      if (libraries.length > 0) {
-        libraries.forEach((lib) => {
-          if (!node.linearizedBaseContracts.includes(lib)) {
-            node.linearizedBaseContracts.push(lib);
+  visitFunctionCall(node: FunctionCall, ast: AST): void {
+    if (node.vExpression instanceof MemberAccess) {
+      const contractDef = new Map();
+
+      node.vExpression.context?.map.forEach((astNode, key) => {
+        if (astNode instanceof ContractDefinition && astNode.kind === ContractKind.Library) {
+          contractDef.set(key, astNode);
+        }
+      });
+
+      const func_id = node.vExpression.referencedDeclaration;
+
+      contractDef.forEach((astNode, key) => {
+        assert(astNode instanceof ContractDefinition);
+        if (astNode.vFunctions.some((func) => func.id === func_id)) {
+          const parent = node.getClosestParentByType(ContractDefinition);
+
+          if (parent === undefined) return;
+
+          if (!parent.linearizedBaseContracts.includes(key)) {
+            parent.linearizedBaseContracts.push(key);
           }
-        });
-      }
+        }
+      });
     }
-    console.log(node.vLinearizedBaseContracts);
     this.commonVisit(node, ast);
   }
 }
