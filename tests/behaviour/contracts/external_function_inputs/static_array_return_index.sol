e0fbfcc4b9db80ad1bd6155250b118231ca9c325
Guy <gprmccomb@gmail.com>
2022-04-13 11:10:04 +0100

    Adding tests

--- tests/behaviour/contracts/external_function_inputs/static_array_return_index.sol
+++ tests/behaviour/contracts/external_function_inputs/static_array_return_index.sol
@@ -4,10 +4,29 @@ pragma solidity ^0.8.0;
 
 contract WARP {
 
+struct StrucDef {
+    uint8 x;
+    uint256 y;
+}
 
-    function testReturnIndex(int16[3] memory listA) pure external returns (int16) {
-        //int16[2] memory listB = [listA[1], listA[2]];
+    function testA(int16[3] memory listA) pure external returns (int16) {
+         return listA[2];
+    }
+
+    function testB(int16[3] memory listA) pure public returns (int16) {
         return listA[2];
     }
+    // Would return y but warp at this stage does not return whole structs.
+    function testC(StrucDef[3] memory listA) pure external returns (uint8) {
+        return listA[2].x;
+    }
+
+    function testD(StrucDef[3] memory listA) pure public returns (uint8) {
+         return listA[2].x;
+    }
+
+    function testE(StrucDef[3] memory listA, uint8 z, uint8[3] memory listB) pure public returns (uint8) {
+        return listA[0].x + listB[2];
+    } 
 
 }
--- tests/behaviour/expectations/behaviour.ts
+++ tests/behaviour/expectations/behaviour.ts
@@ -470,6 +470,35 @@ export const expectations = flatten(
               ],
             ),
           ]),
+          File.Simple('static_array_return_index', [
+            new Expect(
+              'testing a static array of ints can be passed into an external function and written to memory and index returned.',
+              [['testA', ['1', '2', '3'], ['3'], '0']],
+            ),
+            new Expect(
+              'testing a static array of ints can be passed into a public function and written to memory and index returned.',
+              [['testB', ['1', '2', '3'], ['3'], '0']],
+            ),
+            new Expect(
+              'testing a static array of structs can be passed into an external function and written to memory and index returned.',
+              [['testC', ['1', '2', '0 ', '3', '4', '0', '5', '6', '0'], ['5'], '0']],
+            ),
+            new Expect(
+              'testing a static array of structs can be passed into a public function and written to memory and index returned.',
+              [['testD', ['1', '2', '0 ', '3', '4', '0', '5', '6', '0'], ['5'], '0']],
+            ),
+            new Expect(
+              'testing when multiple inputs all of them are written into memory and read correctly.',
+              [
+                [
+                  'testE',
+                  ['1', '2', '0 ', '3', '4', '0', '5', '6', '0', '111', '10', '11', '12'],
+                  ['13'],
+                  '0',
+                ],
+              ],
+            ),
+          ]),
         ]),
         new Dir('external_input_checks', [
           File.Simple('int', [
