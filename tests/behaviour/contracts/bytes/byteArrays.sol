dca6c0b5f4f4d67748dcc81d3c92eaf49d595c29
Gary Tse <tse.rong.gary@gmail.com>
2022-04-18 18:11:28 +0800

    fix sol formatting

--- tests/behaviour/contracts/bytes/byteArrays.sol
+++ tests/behaviour/contracts/bytes/byteArrays.sol
@@ -4,32 +4,32 @@ pragma solidity ^0.7.0;
 contract WARP {
 
   bytes2[3] ba;
-	bytes2[] d;
-	uint constant bbb = 2;
+  bytes2[] d;
+  uint constant bbb = 2;
 
-	constructor() {
-		ba[0] = 0x1234;
-		ba[1] = 0x2345;
-		ba[2] = 0x3456;
-	}
+  constructor() {
+    ba[0] = 0x1234;
+    ba[1] = 0x2345;
+    ba[2] = 0x3456;
+  }
 
   function getC(uint8 i) public view returns (bytes2) {
     return ba[i];
   }
 
-	function getStorageBytesArray() external view returns (bytes2) {
-		return ba[2];
-	}
+  function getStorageBytesArray() external view returns (bytes2) {
+    return ba[2];
+  }
 
-	function getMemoryBytesArray() public pure returns (bytes2) {
-		bytes2[bbb] memory abc;
-		abc[0] = 0x1234;
-		abc[1] = 0x2345;
-		return abc[1];
-	}
+  function getMemoryBytesArray() public pure returns (bytes2) {
+    bytes2[bbb] memory abc;
+    abc[0] = 0x1234;
+    abc[1] = 0x2345;
+    return abc[1];
+  }
 
-	function getStorageBytesDynArray() external returns (bytes2) {
-		d.push(0x1234);
-		return d[0];
-	}
+  function getStorageBytesDynArray() external returns (bytes2) {
+    d.push(0x1234);
+    return d[0];
+  }
 }
