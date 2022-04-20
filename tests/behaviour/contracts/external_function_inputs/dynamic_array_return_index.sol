eeb70202ce6c9a13d57e68a36ef63cf08ece5f32
Guy <gprmccomb@gmail.com>
2022-04-20 14:21:44 +0100

    Prototype

--- tests/behaviour/contracts/external_function_inputs/dynamic_array_return_index.sol
+++ tests/behaviour/contracts/external_function_inputs/dynamic_array_return_index.sol
@@ -3,11 +3,11 @@ pragma solidity ^0.8.6;
 //SPDX-License-Identifier: MIT
 
 contract WARP {
-    function call (uint8 x, uint8[] memory z) pure internal {
-        uint8 z = x;
-    }
-    function test(uint8[] memory x, uint8 y) pure external returns (uint8) {
-    call(y, x);
-        return (x[2]);
+    // function testing (uint8 x, uint8[] memory z) pure internal returns (uint8[] memory) {
+    //     return z;
+    // }
+    function test(uint8[] memory x) pure external returns (uint8) {
+    // uint8[] memory testing_testing = testing(y, x);
+        return (x[0]);
     }
 }
