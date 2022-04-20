4361cba5f1160cd685fd0198ebcb123d0a9d1d1a
Gary Tse <tse.rong.gary@gmail.com>
2022-04-20 12:32:23 +0800

    update example contract expectations

--- src/testing.ts
+++ src/testing.ts
@@ -103,7 +103,7 @@ const expectedResults = new Map<string, ResultType>([
   ['example_contracts/state_variables/arrays', 'CairoCompileFailed'],
   ['example_contracts/state_variables/mappings', 'Success'],
   ['example_contracts/state_variables/structs', 'CairoCompileFailed'],
-  ['example_contracts/state_variables/misc', 'NotSupportedYet'],
+  ['example_contracts/state_variables/misc', 'TranspilationFailed'],
   ['example_contracts/structs', 'Success'],
   ['example_contracts/tupleAssignment7', 'Success'],
   ['example_contracts/tupleAssignment8', 'SolCompileFailed'],
