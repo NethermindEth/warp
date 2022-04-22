a52dcae57f6c6098c75f7c4eb78bb8668404574c
rodrigo-pino <rodrodpino@gmail.com>
2022-04-22 14:31:58 -0400

    Minor change in comment indentation

--- src/passes/publicFunctionSplitter/internalFunctionCallCollector.ts
+++ src/passes/publicFunctionSplitter/internalFunctionCallCollector.ts
@@ -14,8 +14,8 @@ export class InternalFunctionCallCollector extends ASTMapper {
      This class collects all functions which are called from the same contract it is defined in.
      It is used for later to avoid splitting function which are not called from anywhere in the
     contract producing cleaner Cairo code and lesser step count.
-     All public Solidity functions which are not called from the same contract they are defined in
-    will be treated as external only functions.
+     All public Solidity functions which are not called from the same contract they are defined 
+    in will be treated as external only functions.
   */
   constructor(public internalFunctionCallSet: Set<FunctionDefinition>) {
     super();
