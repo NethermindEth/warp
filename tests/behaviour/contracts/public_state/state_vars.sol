0c088433b19e08772109f54f3117cc411e10e616
Rohit Ranjan <rohitrjn629@gmail.com>
2022-04-22 16:29:28 +0530

    fix typestring bug: getter fns

--- tests/behaviour/contracts/public_state/state_vars.sol
+++ tests/behaviour/contracts/public_state/state_vars.sol
@@ -8,8 +8,14 @@ To test whether getters functions are being generated correctly for public state
 
 contract WARP{
     uint256 public x;
+    uint256[2] public y;
+    mapping(uint256 => uint256[5]) public z;
+    uint256[2][2][2] public w;
     constructor() public{
         x = 10;
+        y[0] = 12;
+        z[0][0] = 13;
+        z[0][1] = 14;
+        w[0][0][0] = 15;
     }
-
 }
