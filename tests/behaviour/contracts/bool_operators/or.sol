bfdc7914f360bcdad17dd950c7e0315a45dfec53
rodrigo-pino <rodrodpino@gmail.com>
2022-04-29 19:00:45 -0400

    Fix bug in contracts

--- tests/behaviour/contracts/bool_operators/or.sol
+++ tests/behaviour/contracts/bool_operators/or.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.10;
 //SPDX-License-Identifier: MIT
 
 contract WARP {
-    function test(bool x, bool y) returns (bool) {
+    function test(bool x, bool y) public pure returns (bool) {
         return x || y;
     }
 }
