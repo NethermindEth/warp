6822862f3738f0f7a461fda410092911d07a8c5e
rodrigo-pino <rodrodpino@gmail.com>
2022-05-09 17:50:10 -0400

    Add handling for assignment as expression only

--- src/passes/unloadingAssignment.ts
+++ src/passes/unloadingAssignment.ts
@@ -1,14 +1,78 @@
-import { Assignment, BinaryOperation, Literal, LiteralKind, UnaryOperation } from 'solc-typed-ast';
+import assert from 'assert';
+import {
+  Assignment,
+  BinaryOperation,
+  ContractDefinition,
+  DataLocation,
+  Expression,
+  ExpressionStatement,
+  FunctionDefinition,
+  getNodeType,
+  Identifier,
+  Literal,
+  LiteralKind,
+  Mutability,
+  StateVariableVisibility,
+  UnaryOperation,
+  VariableDeclaration,
+  VariableDeclarationStatement,
+} from 'solc-typed-ast';
 import { AST } from '../ast/ast';
 import { ASTMapper } from '../ast/mapper';
+import { printNode } from '../utils/astPrinter';
 import { cloneASTNode } from '../utils/cloning';
+import { createIdentifier } from '../utils/nodeTemplates';
 import { toHexString } from '../utils/utils';
 
 export class UnloadingAssignment extends ASTMapper {
-  visitAssignment(node: Assignment, ast: AST): void {
-    if (node.operator === '=') return this.visitExpression(node, ast);
-    const lhsValue = cloneASTNode(node.vLeftHandSide, ast);
+  lastUsedTempVar = 0;
 
+  // When assignments are not used as statements each value change is
+  // stored in a temporal var which will hold the value of the assignment
+  // as an expression
+  private handleAssignmentAsExpressionOnly(node: Assignment, ast: AST) {
+    // create a variable declaration equal to the node right hand side
+    // Need to give a unique name
+    assert(
+      node.vLeftHandSide instanceof Identifier,
+      `Assignment left hand side must be an Identifier, intead ${printNode(node)} was found`,
+    );
+
+    let scope = node.getClosestParentByType(FunctionDefinition)?.id;
+    scope = scope === undefined ? node.getClosestParentByType(ContractDefinition)?.id : scope;
+    assert(scope !== undefined, 'Scope for temporal var initialization could not be found');
+
+    const lhs = cloneASTNode(node.vLeftHandSide, ast);
+    const rhs = cloneASTNode(node.vRightHandSide, ast);
+    const tempVarStatement = createVariableDeclaration(
+      `__warp_temp${this.lastUsedTempVar++}`,
+      rhs,
+      scope,
+      ast,
+    );
+    const realVarAssignmet = createAssignmentStatement(
+      '=',
+      lhs,
+      createIdentifier(tempVarStatement.vDeclarations[0], ast),
+      ast,
+    );
+
+    ast.insertStatementBefore(node, tempVarStatement);
+    ast.insertStatementBefore(node, realVarAssignmet);
+    ast.replaceNode(node, createIdentifier(tempVarStatement.vDeclarations[0], ast));
+  }
+
+  visitAssignment(node: Assignment, ast: AST): void {
+    if (node.operator === '=' && !(node.getParents()[0] instanceof ExpressionStatement)) {
+      const inmediateParent = node.getParents()[0];
+      if (!(inmediateParent instanceof ExpressionStatement)) {
+        return this.handleAssignmentAsExpressionOnly(node, ast);
+      }
+
+      return this.visitExpression(node, ast);
+    }
+
+    const lhsValue = cloneASTNode(node.vLeftHandSide, ast);
     // Extract e.g. "+" from "+="
     const operator = node.operator.slice(0, node.operator.length - 1);
     node.operator = '=';
@@ -68,4 +132,37 @@ export class UnloadingAssignment extends ASTMapper {
       this.dispatchVisit(compoundAssignment, ast);
     }
   }
+}
+
+function createVariableDeclaration(name: string, initalValue: Expression, scope: number, ast: AST) {
+  const varDecl = new VariableDeclarationStatement(
+    ast.reserveId(),
+    '',
+    [],
+    [
+      new VariableDeclaration(
+        ast.reserveId(),
+        '',
+        false,
+        false,
+        name,
+        scope,
+        false,
+        DataLocation.Default,
+        StateVariableVisibility.Internal,
+        Mutability.Mutable,
+        initalValue.typeString,
+      ),
+    ],
+    initalValue,
+  );
+  return varDecl;
+}
+
+function createAssignmentStatement(operator: string, lhs: Expression, rhs: Expression, ast: AST) {
+  return new ExpressionStatement(
+    ast.reserveId(),
+    '',
+    new Assignment(ast.reserveId(), '', lhs.typeString, operator, lhs, rhs),
+  );
 }
