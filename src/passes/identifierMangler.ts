8e3e05cc5c734e3a286373916e49a0823ecc219c
rodrigo-pino <rodrodpino@gmail.com>
2022-05-09 10:05:47 -0400

    Modify `identifierMangler` to not modify nameless functions

--- src/passes/identifierMangler.ts
+++ src/passes/identifierMangler.ts
@@ -16,6 +16,7 @@ import {
 import { ABIEncoderVersion } from 'solc-typed-ast/dist/types/abi';
 import { AST } from '../ast/ast';
 import { ASTMapper } from '../ast/mapper';
+import { isNameless } from '../utils/utils';
 
 export class IdentifierMangler extends ASTMapper {
   lastUsedVariableId = 0;
@@ -24,7 +25,9 @@ export class IdentifierMangler extends ASTMapper {
 
   // This strategy should allow checked demangling post transpilation for a more readable result
   createNewExternalFunctionName(fd: FunctionDefinition): string {
-    return `${fd.name}_${fd.canonicalSignatureHash(ABIEncoderVersion.V2)}`;
+    return !isNameless(fd)
+      ? `${fd.name}_${fd.canonicalSignatureHash(ABIEncoderVersion.V2)}`
+      : fd.name;
   }
 
   // This strategy should allow checked demangling post transpilation for a more readable result
