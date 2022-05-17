c7dbae29f11872560bb13b7b52382989c0791f7e
Carmen Irene Cabrera Rodríguez <49727740+cicr99@users.noreply.github.com>
2022-05-17 16:41:52 -0400

    Handle inheritance issues with function and modifier overriding (#344)
    * handle references for overriden functions in inheritance
    
    * fix accessing modifier from base contract
    
    * add new tests for inheritance
    
    * add comments for documentation

--- src/passes/inheritanceInliner/inheritanceInliner.ts
+++ src/passes/inheritanceInliner/inheritanceInliner.ts
@@ -31,16 +31,57 @@ export class InheritanceInliner extends ASTMapper {
       return;
     }
 
+    // The remapping structures are used to update the references to the new members
+    // added in the current contract. The idea is to clone each member from the base
+    // contracts and add it to the current contract, so the pair
+    // (<old_member_id>, <cloned_member>) is stored in the map for each one of them.
+    //
+    // When there is an overriding function (or modifier) all references to the function
+    // should point to this particular function and not the overriden implementation of it.
+    // That is what the overriders maps are used for, they keep the reference to the
+    // overriding member.
+    // Nevertheless, the references to all the cloned members must also be kept, since they
+    // can be called specifying the contract, even if they are being overriden somewhere
+    // else down the line of inheritance.
+    //
+    // Example:
+    //    abstract contract A {
+    //      function f() public view virtual returns (uint256);
+
+    //      function g() public view virtual returns (uint256) {
+    //        return 10;
+    //      }
+    //    }
+
+    //    abstract contract B is A {
+    //      function g() public view override returns (uint256) {
+    //        return f();
+    //      }
+    //    }
+
+    // When analizing contract B, cloned functions f and g will be added, so the
+    // function remappings will look something like:
+    //    functionRemapping = {
+    //      (A.f.id, cloned_f),
+    //      (A.g.id, cloned_g)
+    //    }
+    //    functionRemappingOverriders = {
+    //      (A.f.id, cloned_f),
+    //      (A.g.id, B.g),
+    //      (B.g, B.g)
+    //    }
     const functionRemapping: Map<number, FunctionDefinition> = new Map();
+    const functionRemappingOverriders: Map<number, FunctionDefinition> = new Map();
     const variableRemapping: Map<number, VariableDeclaration> = new Map();
     const modifierRemapping: Map<number, ModifierDefinition> = new Map();
+    const modifierRemappingOverriders: Map<number, ModifierDefinition> = new Map();
     const eventRemapping: Map<number, EventDefinition> = new Map();
 
     solveConstructorInheritance(node, ast, this.generateIndex.bind(this));
-    addPrivateSuperFunctions(node, functionRemapping, ast);
+    addPrivateSuperFunctions(node, functionRemapping, functionRemappingOverriders, ast);
     addNonoverridenPublicFunctions(node, functionRemapping, ast);
     addStorageVariables(node, variableRemapping, ast);
-    addNonOverridenModifiers(node, modifierRemapping, ast);
+    addNonOverridenModifiers(node, modifierRemapping, modifierRemappingOverriders, ast);
     addEventDefintion(node, eventRemapping, ast);
     // Structs from parent classes are added but references inside the contract definition
     // are not updated since this is not enough.
@@ -52,9 +93,9 @@ export class InheritanceInliner extends ASTMapper {
     // By just adding the structs this issues are avoided and valid cairo code is produced.
     addStructDefinition(node, ast);
 
-    updateReferencedDeclarations(node, functionRemapping, ast);
-    updateReferencedDeclarations(node, variableRemapping, ast);
-    updateReferencedDeclarations(node, modifierRemapping, ast);
+    updateReferencedDeclarations(node, functionRemapping, functionRemappingOverriders, ast);
+    updateReferencedDeclarations(node, variableRemapping, variableRemapping, ast);
+    updateReferencedDeclarations(node, modifierRemapping, modifierRemappingOverriders, ast);
     updateReferenceEmitStatemets(node, eventRemapping, ast);
 
     removeBaseContractDependence(node);
--- src/passes/inheritanceInliner/modifiersInheritance.ts
+++ src/passes/inheritanceInliner/modifiersInheritance.ts
@@ -1,4 +1,3 @@
-import assert from 'assert';
 import { ContractDefinition, ModifierDefinition } from 'solc-typed-ast';
 import { AST } from '../../ast/ast';
 import { cloneASTNode } from '../../utils/cloning';
@@ -7,31 +6,26 @@ import { getBaseContracts } from './utils';
 export function addNonOverridenModifiers(
   node: ContractDefinition,
   idRemapping: Map<number, ModifierDefinition>,
+  idRemappingOverriders: Map<number, ModifierDefinition>,
   ast: AST,
 ) {
-  const modifierNames: Set<string> = new Set();
   const currentModifiers: Map<string, ModifierDefinition> = new Map();
 
-  node.vModifiers.forEach((modifier) => {
-    modifierNames.add(modifier.name);
-    currentModifiers.set(modifier.name, modifier);
-  });
+  node.vModifiers.forEach((modifier) => currentModifiers.set(modifier.name, modifier));
 
   getBaseContracts(node).forEach((contract) => {
-    contract.vModifiers.forEach((modifier) => {
-      const sz = modifierNames.size;
-      modifierNames.add(modifier.name);
-      if (modifierNames.size > sz) {
-        const clonedModifier = cloneASTNode(modifier, ast);
+    contract.vModifiers.forEach((modifier, depth) => {
+      const exisitingModifier = currentModifiers.get(modifier.name);
+      const clonedModifier = cloneASTNode(modifier, ast);
+      idRemapping.set(modifier.id, clonedModifier);
+      if (exisitingModifier === undefined) {
         currentModifiers.set(modifier.name, clonedModifier);
-        node.appendChild(clonedModifier);
+        idRemappingOverriders.set(modifier.id, clonedModifier);
+      } else {
+        clonedModifier.name = `${clonedModifier.name}_m${depth + 1}`;
+        idRemappingOverriders.set(modifier.id, exisitingModifier);
       }
-      const mod = currentModifiers.get(modifier.name);
-      assert(
-        mod instanceof ModifierDefinition,
-        'Modifier should have been already added to the map',
-      );
-      idRemapping.set(modifier.id, mod);
+      node.appendChild(clonedModifier);
     });
   });
 }
