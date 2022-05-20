bfea164b2b8765e5411b842c14efdc071226534b
rodrigo-pino <rodrodpino@gmail.com>
2022-05-20 15:14:03 -0400

    DeleteHandler does not modify deletion to storage

--- src/passes/deleteHandler.ts
+++ src/passes/deleteHandler.ts
@@ -1,37 +1,50 @@
 import {
   Assignment,
+  DataLocation,
   ExpressionStatement,
   getNodeType,
-  MappingType,
+  Identifier,
+  PointerType,
   Return,
   TupleType,
   UnaryOperation,
+  VariableDeclaration,
 } from 'solc-typed-ast';
 import { AST } from '../ast/ast';
 import { ASTMapper } from '../ast/mapper';
+import { printNode } from '../utils/astPrinter';
 import { getDefaultValue } from '../utils/defaultValueNodes';
 
 export class DeleteHandler extends ASTMapper {
   visitUnaryOperation(node: UnaryOperation, ast: AST): void {
-    if (node.operator === 'delete') {
-      const nodeType = getNodeType(node.vSubExpression, ast.compilerVersion);
-      const newNode = getDefaultValue(nodeType, node, ast);
-      if (nodeType instanceof MappingType) ast.replaceNode(node, newNode);
-      else {
-        ast.replaceNode(
-          node,
-          new Assignment(
-            ast.reserveId(),
-            node.src,
-            nodeType.pp(),
-            '=',
-            node.vSubExpression,
-            newNode,
-            node.raw,
-          ),
-        );
-      }
-    } else this.commonVisit(node, ast);
+    if (node.operator !== 'delete') {
+      return this.commonVisit(node, ast);
+    }
+
+    const nodeType = getNodeType(node.vSubExpression, ast.compilerVersion);
+    // Deletetion from storage is handled in References
+    if (
+      (nodeType instanceof PointerType && nodeType.location === DataLocation.Storage) ||
+      (node instanceof Identifier &&
+        node.vReferencedDeclaration instanceof VariableDeclaration &&
+        node.vReferencedDeclaration.stateVariable)
+    ) {
+      return;
+    }
+
+    const newNode = getDefaultValue(nodeType, node, ast);
+    ast.replaceNode(
+      node,
+      new Assignment(
+        ast.reserveId(),
+        node.src,
+        nodeType.pp(),
+        '=',
+        node.vSubExpression,
+        newNode,
+        node.raw,
+      ),
+    );
   }
 
   visitReturn(node: Return, ast: AST): void {
@@ -39,6 +52,7 @@ export class DeleteHandler extends ASTMapper {
     if (node.vExpression) {
       const nodeType = getNodeType(node.vExpression, ast.compilerVersion);
       if (nodeType instanceof TupleType && nodeType.getChildren().length === 0) {
+        console.log(printNode(node));
         const statement = new ExpressionStatement(
           ast.reserveId(),
           node.src,
