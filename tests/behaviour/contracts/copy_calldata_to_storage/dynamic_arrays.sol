04e21a63bb143aaeca95683d78117d71f6b8d5a7
rodrigo-pino <rodrodpino@gmail.com>
2022-05-24 19:40:49 -0400

    Minor change

--- tests/behaviour/contracts/copy_calldata_to_storage/dynamic_arrays.sol
+++ tests/behaviour/contracts/copy_calldata_to_storage/dynamic_arrays.sol
@@ -2,14 +2,8 @@
 pragma solidity >=0.4.0 <0.9.0;
 
 contract WARP {
-    struct S {
-        uint8 x;
-        uint8 y;
-    }
-
     uint8[] x;
     uint[] y;
-    S[] z;
 
     function setX(uint8[] calldata a) public {
         x = a;
@@ -18,10 +12,6 @@ contract WARP {
     function setY(uint[] calldata a) public {
         y = a;
     }
-
-    // function t3(S[] calldata a) public {
-        // z = a;
-    // }
 
     function getX() public view returns (uint8) {
         return x[0] + x[1] + x[2];
