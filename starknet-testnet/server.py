0e2f6e2e5b737bd311cfee9f7c7f1c5d9bd13822
Rohit Ranjan <rohitrjn629@gmail.com>
2022-06-01 22:40:53 +0530

    fix lint

--- starknet-testnet/server.py
+++ starknet-testnet/server.py
@@ -105,7 +105,10 @@ async def invoke():
         )
     except StarkException as err:
         print(err)
-        if err.code == StarknetErrorCode.TRANSACTION_FAILED or err.code == StarknetErrorCode.OUT_OF_RESOURCES:
+        if (
+            err.code == StarknetErrorCode.TRANSACTION_FAILED
+            or err.code == StarknetErrorCode.OUT_OF_RESOURCES
+        ):
             return jsonify(
                 {"transaction_info": {"threw": True, "message": err.message}}
             )
