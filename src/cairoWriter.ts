b9a801297f91e398f65e1161cbdb1d96138d1603
Ahmet Yazıcı <75089142+yaziciahmet@users.noreply.github.com>
2022-06-02 14:56:08 +0300

    Return index access bug (#422)
    * Calldata assignment after tuple split solved.
    
    * Behaviour test is added
    
    * Returning index access bug fixed

--- src/cairoWriter.ts
+++ src/cairoWriter.ts
@@ -713,7 +713,8 @@ class IdentifierWriter extends CairoASTNodeWriter {
       isDynamicCallDataArray(getNodeType(node, this.ast.compilerVersion)) &&
       ((node.getClosestParentByType(Return) !== undefined &&
         node.getClosestParentByType(FunctionDefinition)?.visibility ===
-          FunctionVisibility.External) ||
+          FunctionVisibility.External &&
+        node.getClosestParentByType(IndexAccess) === undefined) ||
         (node.parent instanceof FunctionCall &&
           node.parent.vReferencedDeclaration instanceof FunctionDefinition &&
           node.parent.vReferencedDeclaration.visibility === FunctionVisibility.External))
