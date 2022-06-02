ebb356fda7b7d5789da13ceb0b1a1cd208048755
Swapnil Raj <raj.swapnil99@gmail.com>
2022-06-02 18:44:58 +0100

    Fix expectations

--- src/testing.ts
+++ src/testing.ts
@@ -189,8 +189,10 @@ const expectedResults = new Map<string, ResultType>(
     ['example_contracts/units', 'Success'],
     ['example_contracts/unsupportedFunctions/abi', `WillNotSupport`],
     ['example_contracts/unsupportedFunctions/keccak256', `WillNotSupport`],
-    ['example_contracts/unsupportedFunctions/ecrecover', `WillNotSupport`],
+    ['example_contracts/unsupportedFunctions/ecrecover', `Success`],
     ['example_contracts/unsupportedFunctions/addmod', `WillNotSupport`],
+    // Supported precompiles
+    ['example_contracts/precompiles/ecrecover.sol', 'Success'],
     // Uses bytes memory
     ['example_contracts/unsupportedFunctions/shadowAbi', `Success`],
     // Uses bytes memory
