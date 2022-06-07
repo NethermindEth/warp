f0003e2e47379c30ce6cc208200f8368a20ddcdf
Dom Henderson <dominic@nethermind.io>
2022-06-07 21:40:09 +0100

    Fix cairo naming bug

--- src/cairoUtilFuncGen/memory/memoryToStorage.ts
+++ src/cairoUtilFuncGen/memory/memoryToStorage.ts
@@ -153,14 +153,14 @@ export class MemoryToStorageGen extends StringIndexedFuncGen {
     let copyCode: string;
     if (type.elementT instanceof ArrayType && type.elementT.size === undefined) {
       copyCode = [
-        `    let (elemName) = readId(loc)`,
+        `    let (elemName) = readId(storage_loc)`,
         `    let (read) = dict_read{dict_ptr=warp_memory}(mem_loc)`,
         `    ${this.getOrCreate(type.elementT)}(elemName, read)`,
       ].join('\n');
     } else if (isComplexType(type.elementT)) {
       copyCode = [
         `    let (read) = dict_read{dict_ptr=warp_memory}(mem_loc)`,
-        `    ${this.getOrCreate(type.elementT)}(loc, read)`,
+        `    ${this.getOrCreate(type.elementT)}(storage_loc, read)`,
       ].join('\n');
     } else {
       copyCode = mapRange(elementStorageWidth, (n) =>
