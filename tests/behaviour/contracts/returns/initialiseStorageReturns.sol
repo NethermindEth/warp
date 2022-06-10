2a53b46554c95c16d6a831e4ff3224d40c414cc5
Dom Henderson <dominic@nethermind.io>
2022-06-10 21:04:07 +0100

    Use placeholder initialiser for storage pointers

---
+++ tests/behaviour/contracts/returns/initialiseStorageReturns.sol
@@ -0,0 +1,11 @@
+contract WARP {
+	uint[] arr1;
+	uint[][] arr2;
+	function getStorageArrays() internal returns (uint[] storage ptr1, uint[][] storage ptr2) {
+		ptr1 = arr1;
+		ptr2 = arr2;
+	}
+	function getDefaultArrayLengths() public returns (uint, uint) {
+		return (arr1.length, arr2.length);
+	}
+}
