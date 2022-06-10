38e37b1a0ade03c500207b171a678a1121ad97d0
Swapnil Raj <swapnilraj@users.noreply.github.com>
2022-06-10 18:20:12 +0100

    Reject cairo keywords (#481)

---
+++ example_contracts/rejectedTerms/contract_name_lib.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.6;
+
+struct S {
+  uint256 a;
+}
+
+contract Lib$$ {
+
+  function mybalance() pure internal returns (S memory) {
+    return S(110);
+  }
+}
+
--- src/passes/identifierManglerPass/declarationNameMangler.ts
+++ src/passes/identifierManglerPass/declarationNameMangler.ts
@@ -5,11 +5,68 @@ import {
   FunctionVisibility,
   SourceUnit,
   ContractDefinition,
+  ASTNode,
 } from 'solc-typed-ast';
 import { ABIEncoderVersion } from 'solc-typed-ast/dist/types/abi';
 import { AST } from '../../ast/ast';
 import { ASTMapper } from '../../ast/mapper';
+import { printNode } from '../../utils/astPrinter';
+import { WillNotSupportError } from '../../utils/errors';
 import { isNameless } from '../../utils/utils';
+
+// Terms grabbed from here
+// https://github.com/starkware-libs/cairo-lang/blob/master/src/starkware/cairo/lang/compiler/cairo.ebnf
+export const reservedTerms = [
+  'ret',
+  'return',
+  'using',
+  'jmp',
+  'alloc_locals',
+  'rel',
+  'func',
+  'end',
+  'nondet',
+  'felt',
+  'codeoffset',
+  'Uint256',
+  'cast',
+  'ap',
+  'fp',
+  'dw',
+  '%lang',
+  '%builtins',
+  'with_attr',
+  'static_assert',
+  'assert',
+  'member',
+  'new',
+  'call',
+  'abs',
+  'as',
+  'from',
+  'local',
+  'let',
+  'tempvar',
+  'const',
+  'struct',
+  'namespace',
+];
+
+const unsupportedCharacters = ['$'];
+
+export function checkSourceTerms(term: string, node: ASTNode) {
+  if (reservedTerms.includes(term)) {
+    throw new WillNotSupportError(`${printNode(node)} contains ${term} which is a cairo keyword`);
+  }
+
+  unsupportedCharacters.forEach((c: string) => {
+    if (term.includes(c)) {
+      throw new WillNotSupportError(
+        `${printNode(node)} ${term} contains unsupported character ${c}`,
+      );
+    }
+  });
+}
 
 export class DeclarationNameMangler extends ASTMapper {
   lastUsedVariableId = 0;
@@ -53,6 +110,7 @@ export class DeclarationNameMangler extends ASTMapper {
     node.name = this.createNewVariableName(node.name);
   }
   mangleStructDefinition(node: StructDefinition): void {
+    checkSourceTerms(node.name, node);
     node.vMembers.forEach((m) => this.mangleVariableDeclaration(m));
   }
   mangleFunctionDefinition(node: FunctionDefinition): void {
@@ -69,6 +127,7 @@ export class DeclarationNameMangler extends ASTMapper {
     }
   }
   mangleContractDefinition(node: ContractDefinition): void {
+    checkSourceTerms(node.name, node);
     node.vStructs.forEach((s) => this.mangleStructDefinition(s));
     node.vFunctions.forEach((n) => this.mangleFunctionDefinition(n));
     node.vStateVariables.forEach((v) => this.mangleVariableDeclaration(v));
