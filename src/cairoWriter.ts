2d1a7e8d2edd7bc925ccd8e9fb53369777f08c69
Dom Henderson <dominic@nethermind.io>
2022-06-11 01:47:37 +0100

    Handle variableDeclarationStatements with gaps

--- src/cairoWriter.ts
+++ src/cairoWriter.ts
@@ -198,6 +198,7 @@ class VariableDeclarationWriter extends CairoASTNodeWriter {
 }
 
 class VariableDeclarationStatementWriter extends CairoASTNodeWriter {
+  gapVarCounter = 0;
   writeInner(node: VariableDeclarationStatement, writer: ASTWriter): SrcDesc {
     assert(
       node.vInitialValue !== undefined,
@@ -206,10 +207,9 @@ class VariableDeclarationStatementWriter extends CairoASTNodeWriter {
 
     const documentation = getDocumentation(node.documentation, writer);
     const declarations = node.assignments.flatMap((id) => {
-      if (id === null)
-        throw new NotSupportedYetError(
-          `VariableDeclarationStatements with gaps not implemented yet`,
-        );
+      if (id === null) {
+        return ['__warp_gv'];
+      }
       const declaration = node.vDeclarations.find((decl) => decl.id === id);
       assert(declaration !== undefined, `Unable to find variable declaration for assignment ${id}`);
       const type = generalizeType(getNodeType(declaration, this.ast.compilerVersion))[0];
