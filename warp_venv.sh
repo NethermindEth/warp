65863d9bd2c322ceed7ccd7305d25b87eeffb536
Swapnil Raj <raj.swapnil99@gmail.com>
2022-06-13 13:00:10 +0100

    Debug

--- warp_venv.sh
+++ warp_venv.sh
@@ -5,6 +5,8 @@ SCRIPT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 echo $SCRIPT_DIR
 echo $SCRIPT_DIR
 echo $SCRIPT_DIR
+echo $PWD
+pwd
 
 $PYTHON_BIN -m venv "$SCRIPT_DIR"/warp_venv
 . $SCRIPT_DIR/warp_venv/bin/activate
