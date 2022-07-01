3330c946109a89749332fa65c2a80a47492c3336
Guy <gprmccomb@gmail.com>
2022-07-01 17:57:27 +0100

    note

---
+++ a.sol
@@ -0,0 +1,18 @@
+pragma solidity 0.8.14;
+
+// SPDX-License-Identifier: MIT
+
+// contract M {
+//     function multiply(a, b) pure external {
+//         a+b;
+//     }
+// }
+
+contract WARP {
+
+    function f(uint16 y) pure external {
+        
+       uint8 x = uint8(y);
+    //    M.multiply(10, 5);
+    }
+}
