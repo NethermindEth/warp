9e3100bab5a9a87298caa974fdd108ccb21e949d
Guy <gprmccomb@gmail.com>
2022-07-12 17:30:31 +0100

    Changing tests

--- tests/behaviour/contracts/public_func_splitter/value_passing.sol
+++ tests/behaviour/contracts/public_func_splitter/value_passing.sol
@@ -1,45 +1,49 @@
 pragma solidity ^0.8.0;
 
 //SPDX-License-Identifier: MIT
+struct s1 {
+  uint8 x;
+  uint8 y;
+}
 
 contract A {
-  uint8 a = 8;
-  
-  function f(uint8 a) pure public returns (uint8) {
-    uint8 b = a;
-    uint8 a = 25;
-    return a;
+
+  function modifyStructCrossContract(s1 memory memoryStruct) pure public returns (uint8) {
+    memoryStruct.x = 10;
+    memoryStruct.y = 11;
   }
 
 }
 
 contract WARP {
-  uint8 a = 3;
-  
-  function f(uint8 a) pure public returns (uint8) {
-    uint8 b = a;
-    uint8 a = 15;
-    return a;
+
+  function modifyMemoryStruct(s1 memory memoryStruct) pure public {
+    memoryStruct.x = 10;
+    memoryStruct.y = 11;
   }
 
-  function internalCallIdentifier(uint8 a) pure public returns (uint8) {
-   uint8 c = f(a);
-   return c;
-   }
+  function testExternalCallMemberAccess() view external returns (s1 memory) {
+    s1 memory originalStruct = s1(1, 2);
+    this.modifyMemoryStruct(originalStruct);
+    return originalStruct;
+  }
 
- function internalCallMemberAccess(uint8 a) pure public returns (uint8) {
-   uint8 c = WARP.f(a);
-   return c;
-   }
+  function testInternalCallMemberAccess() pure external returns (s1 memory) {
+    s1 memory originalStruct = s1(1, 2);
+    WARP.modifyMemoryStruct(originalStruct);
+    return originalStruct;
+  }
 
- function externalCallMemberAccess(uint8 a) view public returns (uint8) {
-   uint8 c = this.f(a);
-   return c;
-   }
+  function testInternalCallIdentifier() pure external returns (s1 memory) {
+    s1 memory originalStruct = s1(1, 2);
+    modifyMemoryStruct(originalStruct);
+    return originalStruct;
+  }
 
-  function externalCrossContract(address addr) pure public returns (uint8) {
-    A crossContract = A(addr);
-    uint8 ret = crossContract.f(10);
-    return ret;
+    function testExternalCallCrossContract(address addr) pure public returns (s1 memory) {
+    A a = A(addr);
+    s1 memory originalStruct = s1(1, 2);
+    a.modifyStructCrossContract(originalStruct);
+    return originalStruct;
   }
 }
--- tests/behaviour/expectations/behaviour.ts
+++ tests/behaviour/expectations/behaviour.ts
@@ -3254,14 +3254,22 @@ export const expectations = flatten(
           File.Simple(
             'value_passing',
             [
-              Expect.Simple('internalCallIdentifier', ['1'], ['15']),
-              Expect.Simple('internalCallMemberAccess', ['1'], ['15']),
-              Expect.Simple('externalCallMemberAccess', ['1'], ['15']),
+              // Expect.Simple('internalCallIdentifier', ['1'], ['15']),
+              Expect.Simple('testExternalCallMemberAccess', [], ['1', '2']),
+              Expect.Simple('testInternalCallMemberAccess', [], ['10', '11']),
+              Expect.Simple('testInternalCallIdentifier', [], ['10', '11']),
               Expect.Simple(
-                'externalCrossContract',
+                'testExternalCallCrossContract',
                 ['address@tests/behaviour/contracts/public_func_splitter/value_passing.A'],
-                ['25'],
+                ['1', '2'],
               ),
+              // Expect.Simple('internalCallMemberAccess', ['1'], ['15']),
+              // Expect.Simple('externalCallMemberAccess', ['1'], ['15']),
+              // Expect.Simple(
+              // 'externalCrossContract',
+              // ['address@tests/behaviour/contracts/public_func_splitter/value_passing.A'],
+              // ['25'],
+              // ),
             ],
             'WARP',
           ),
