2da683ffb42571e1782102fa5b264972449e2560
Guy <gprmccomb@gmail.com>
2022-07-12 18:15:05 +0100

    Modifying TransferHelper & V3Pool

--- v3-core/contracts/UniswapV3Pool.sol
+++ v3-core/contracts/UniswapV3Pool.sol
@@ -138,21 +138,15 @@ contract UniswapV3Pool is IUniswapV3Pool, NoDelegateCall {
     /// @dev This function is gas optimized to avoid a redundant extcodesize check in addition to the returndatasize
     /// check
     function balance0() private view returns (uint256) {
-        (bool success, bytes memory data) =
-            token0.staticcall(abi.encodeWithSelector(IERC20Minimal.balanceOf.selector, address(this)));
-        require(success && data.length >= 32);
-        return abi.decode(data, (uint256));
+        return IERC20Minimal(token0).balanceOf(address(this));
     }
 
     /// @dev Get the pool's balance of token1
     /// @dev This function is gas optimized to avoid a redundant extcodesize check in addition to the returndatasize
     /// check
     function balance1() private view returns (uint256) {
-        (bool success, bytes memory data) =
-            token1.staticcall(abi.encodeWithSelector(IERC20Minimal.balanceOf.selector, address(this)));
-        require(success && data.length >= 32);
-        return abi.decode(data, (uint256));
-    }
+        return IERC20Minimal(token1).balanceOf(address(this));
+}
 
     /// @inheritdoc IUniswapV3PoolDerivedState
     function snapshotCumulativesInside(int24 tickLower, int24 tickUpper)
