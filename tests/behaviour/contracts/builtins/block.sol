6f151b0b9a7a099e52f572d4a40c58f4917055f5
Guy <gprmccomb@gmail.com>
2022-07-19 11:04:29 +0100

    changing tests

--- tests/behaviour/contracts/builtins/block.sol
+++ tests/behaviour/contracts/builtins/block.sol
@@ -5,13 +5,13 @@ pragma solidity >=0.5.0;
 contract WARP {
 
     function number() external view returns (bool) {
-        uint248 y = uint248(block.number);
+        uint256 y = block.number;
         return y > 0;
     }
  
     // Testnet returns 0 for get_block_timestamp so there is no test.
     function timestamp() external view returns (uint248) {
-        uint248 y = uint248(block.timestamp);
+        uint256 y = block.timestamp;
         return y; 
     }
 }
