d156a3f9c864de02cda916d274afa6df0e1e7081
rodrigo-pino <rodrodpino@gmail.com>
2022-07-20 09:19:10 -0400

    Add comments for class

--- src/passes/newToDeploy.ts
+++ src/passes/newToDeploy.ts
@@ -31,6 +31,32 @@ import { CONTRACT_INFIX } from '../utils/nameModifiers';
 import { printNode } from '../utils/astPrinter';
 import { TranspileFailedError } from '../utils/errors';
 
+/** Pass that takes all expressions of the form:
+ *
+ *   `new {salt: salt} Contract(arg1, ..., argn)`
+ *
+ *   And transpiles it to:
+ *
+ *   `deploy(Contract_class_hash, salt, encode(arg1, ... argn))`
+ *
+ *   -----
+ *
+ *   Since solidity stores the bytecode of the contract to create, and cairo uses
+ *   a declaration address(`class_hash`) of the contract on starknet and the latter
+ *   can only be known when interacting with starknet, empty placeholders are used
+ *   for compiling, and then filled during deployement with the right address.
+ *
+ *   Salt in solidity is 32 bytes while in Cairo is a felt, so it'll be safely
+ *   narrowed down. Notice that values bigger than a felt will result in errors
+ *   such as "abc" which in bytes32 representation gets 0x636466...0000 which is
+ *   bigger than a felt.
+ *
+ *   Encode is a util function generated which takes all the transpiled arguments
+ *   and makes them a big dynamic arrays of felts. For example arguments:
+ *   (a : Uint256, b : felt, c : (felt, felt, felt))
+ *   are encoded as (6, [a.low, a.high, b, c[0], c[1], c[2]])
+ *
+ */
 export class NewToDeploy extends ASTMapper {
   addInitialPassPrerequisites(): void {
     const passKeys: Set<string> = new Set<string>([
@@ -132,8 +158,6 @@ export class NewToDeploy extends ASTMapper {
       { acceptsUnpackedStructArray: true },
     );
     ast.registerImport(node, 'starkware.starknet.common.syscalls', 'deploy');
-
-    // Get salt value
 
     const encodedArguments = ast.getUtilFuncGen(node).utils.encodeAsFelt.gen(node);
     return createCallToFunction(
