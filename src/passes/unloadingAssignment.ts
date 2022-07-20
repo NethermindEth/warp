48b24bc9605323576eb5fe1bf2f4442aa7a4c984
Dom Henderson <dominic@nethermind.io>
2022-07-20 09:44:40 +0100

    Extract subexpressions from compound assignments

--- src/passes/unloadingAssignment.ts
+++ src/passes/unloadingAssignment.ts
@@ -1,8 +1,24 @@
-import { Assignment, BinaryOperation, UnaryOperation } from 'solc-typed-ast';
+import assert from 'assert';
+import {
+  Assignment,
+  BinaryOperation,
+  Expression,
+  FunctionCall,
+  getNodeType,
+  IndexAccess,
+  Literal,
+  MemberAccess,
+  TupleExpression,
+  UnaryOperation,
+  VariableDeclarationStatement,
+} from 'solc-typed-ast';
 import { AST } from '../ast/ast';
 import { ASTMapper } from '../ast/mapper';
+import { printNode } from '../utils/astPrinter';
 import { cloneASTNode } from '../utils/cloning';
+import { COMPOUND_ASSIGNMENT_SUBEXPRESSION_PREFIX } from '../utils/nameModifiers';
 import { createNumberLiteral } from '../utils/nodeTemplates';
+import { typeNameFromTypeNode } from '../utils/utils';
 
 export class UnloadingAssignment extends ASTMapper {
   visitAssignment(node: Assignment, ast: AST): void {
@@ -10,6 +26,9 @@ export class UnloadingAssignment extends ASTMapper {
       return this.visitExpression(node, ast);
     }
 
+    this.extractUnstableSubexpressions(node.vLeftHandSide, ast).forEach((decl) =>
+      this.dispatchVisit(decl, ast),
+    );
     const lhsValue = cloneASTNode(node.vLeftHandSide, ast);
     // Extract e.g. "+" from "+="
     const operator = node.operator.slice(0, node.operator.length - 1);
@@ -62,5 +81,49 @@ export class UnloadingAssignment extends ASTMapper {
       ast.replaceNode(node, compoundAssignment);
       this.dispatchVisit(compoundAssignment, ast);
     }
+  }
+
+  // node is the lvalue of a compound assignment
+  // this function extracts subexpressions that are not guaranteed to produce the same result every time
+  // to constants to evaluate once before the assignment
+  // this is needed because extracting to a 'constant' and then assigning to it would not modify the correct variable
+  // we also extract non-trivial expressions so they're only calculated once, though this is not technically necessary
+  // an array of created statements are returned so they can be visited
+  private extractUnstableSubexpressions(
+    node: Expression,
+    ast: AST,
+  ): VariableDeclarationStatement[] {
+    if (node instanceof IndexAccess) {
+      // a[i++] += 1 -> x = i++; a[x] = a[x] + 1
+      assert(
+        node.vIndexExpression !== undefined,
+        `Unexpected empty index access in compound assignment ${printNode(node)}`,
+      );
+      if (!(node.vIndexExpression instanceof Literal)) {
+        const decl = this.extractIndividualSubExpression(node.vIndexExpression, ast);
+        return [decl, ...this.extractUnstableSubexpressions(node.vBaseExpression, ast)];
+      }
+    } else if (node instanceof MemberAccess) {
+      return this.extractUnstableSubexpressions(node.vExpression, ast);
+    } else if (node instanceof FunctionCall) {
+      return [this.extractIndividualSubExpression(node, ast)];
+    } else if (node instanceof TupleExpression) {
+      assert(
+        node.isInlineArray,
+        `unexpected non-inline-array tuple expression in compound assignment ${printNode(node)}`,
+      );
+      return [this.extractIndividualSubExpression(node, ast)];
+    }
+
+    return [];
+  }
+
+  counter = 0;
+  private extractIndividualSubExpression(node: Expression, ast: AST): VariableDeclarationStatement {
+    return ast.extractToConstant(
+      node,
+      typeNameFromTypeNode(getNodeType(node, ast.compilerVersion), ast),
+      `${COMPOUND_ASSIGNMENT_SUBEXPRESSION_PREFIX}${this.counter++}`,
+    )[1];
   }
 }
