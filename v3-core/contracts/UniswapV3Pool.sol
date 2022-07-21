7e6ef927c359bf42b0429dd81df40f74d5f478d8
rodrigo-pino <rodrodpino@gmail.com>
2022-07-21 13:05:48 -0400

    Fix math bugs

--- v3-core/contracts/UniswapV3Pool.sol
+++ v3-core/contracts/UniswapV3Pool.sol
@@ -462,7 +462,7 @@ contract UniswapV3Pool is IUniswapV3Pool, NoDelegateCall {
                     owner: recipient,
                     tickLower: tickLower,
                     tickUpper: tickUpper,
-                    liquidityDelta: int256(amount).toInt128()
+                    liquidityDelta: int256(uint256(amount)).toInt128()
                 })
             );
 
@@ -519,7 +519,7 @@ contract UniswapV3Pool is IUniswapV3Pool, NoDelegateCall {
                     owner: msg.sender,
                     tickLower: tickLower,
                     tickUpper: tickUpper,
-                    liquidityDelta: -int256(amount).toInt128()
+                    liquidityDelta: -int256(uint256(amount)).toInt128()
                 })
             );
 
