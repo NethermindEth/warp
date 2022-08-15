b6a1e6d112b4c0183dee9220dc46829287e79561
Carmen Cabrera <kr1000a@gmail.com>
2022-08-15 08:53:42 -0400

    adding pass prerequisites

--- src/passes/functionPruner/index.ts
+++ src/passes/functionPruner/index.ts
@@ -4,6 +4,16 @@ import { CallGraphBuilder } from './callGraph';
 import { FunctionRemover } from './functionRemover';
 
 export class FunctionPruner extends ASTMapper {
+  // Function to add passes that should have been run before this pass
+  addInitialPassPrerequisites(): void {
+    const passKeys: Set<string> = new Set<string>([
+      // UnreachableStatementPruner removes statements from the code, which could be
+      // function calls, so this pass should happen after to not include them.
+      'Us',
+    ]);
+    passKeys.forEach((key) => this.addPassPrerequisite(key));
+  }
+
   static map(ast: AST): AST {
     ast.roots.forEach((root) => {
       const graph = new CallGraphBuilder();
