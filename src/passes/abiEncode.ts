506938c4e91f8e06ed1654eac9029b23948354bb
rodrigo-pino <rodrodpino@gmail.com>
2022-09-01 07:56:13 -0400

    [wip] warning for addresses

--- src/passes/abiEncode.ts
+++ src/passes/abiEncode.ts
@@ -1,10 +1,29 @@
-import { ExternalReferenceType, FunctionCall, FunctionCallKind } from 'solc-typed-ast';
+import { ASTNode, ExternalReferenceType, FunctionCall, FunctionCallKind } from 'solc-typed-ast';
 import { AST } from '../ast/ast';
 import { ASTMapper } from '../ast/mapper';
 import { TranspileFailedError } from '../utils/errors';
 
-// Scan the code for function
+/**
+ * Swap any builtin call to abi.encode | encodePacked | encodeWithSelector | encodeWithSignature
+ * for a cairo generated one.
+ *
+ * Warning:
+ *
+ * Special care needs to be taken when encoding addresses since in Ethereum they
+ * take 20 byte but in Starknet they take the whole felt space.
+ * An address whose size fits in 20 bytes will have the exact same encoding
+ * in both the Solidity and Cairo transpiled contract when encoding it in any
+ * form (packed or not).
+ * If the address size is bigger than 20 bytes then:
+ *   - `encodePacked` will only encode its first 20 bytes and any extra would be
+ *   lost
+ *   - `encode` will behave normally since it will have the whole 32 byte slot to
+ *   encode, but if tried to decode as an address in Ethereum an exception will be
+ *   thrown since it won't fit in a Solidity address type
+ */
 export class ABIEncode extends ASTMapper {
+  addressEncodings = new Set<ASTNode>();
+
   // Function to add passes that should have been run before this pass
   addInitialPassPrerequisites(): void {
     const passKeys: Set<string> = new Set<string>([
@@ -41,7 +60,17 @@ export class ABIEncode extends ASTMapper {
       default:
         throw new TranspileFailedError(`Unknown abi function: ${node.vFunctionName}`);
     }
+    // node.vArguments.filter((arg) => ())
+
     ast.replaceNode(node, replacement);
     this.visitExpression(node, ast);
+  }
+
+  static map(ast: AST): AST {
+    const mapper = new this();
+    ast.roots.forEach((root) => {
+      mapper.dispatchVisit(root, ast);
+    });
+    return ast;
   }
 }
