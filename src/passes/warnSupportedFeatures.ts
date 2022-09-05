5634f7b001b734bd9e0326c6114b6f60613ed7f5
rodrigo-pino <rodrodpino@gmail.com>
2022-09-05 15:26:09 -0400

    [wip] warnings

--- src/passes/warnSupportedFeatures.ts
+++ src/passes/warnSupportedFeatures.ts
@@ -1,50 +1,64 @@
+import fs from 'fs';
 import {
   AddressType,
   ASTNode,
   ExternalReferenceType,
   FunctionCall,
   FunctionCallKind,
+  parseSourceLocation,
+  SourceLocation,
 } from 'solc-typed-ast';
 import { AST } from '../ast/ast';
 import { ASTMapper } from '../ast/mapper';
-import { getSourceCode } from '../utils/errors';
 import { warning } from '../utils/formatting';
 import { safeGetNodeType } from '../utils/nodeTypeProcessing';
+import { getSourceFromLocation } from '../utils/utils';
 
 export class WarnSupportedFeatures extends ASTMapper {
-  public addressesToAbiEncode: [string, ASTNode][] = [];
+  public addressesToAbiEncode: ASTNode[] = [];
 
   visitFunctionCall(node: FunctionCall, ast: AST): void {
     if (
       node.kind === FunctionCallKind.FunctionCall &&
       node.vFunctionCallType === ExternalReferenceType.Builtin &&
-      ['encodePacked', 'encode', 'encodeWithSelector', 'encodeWithSignature'].includes(
-        node.vFunctionName,
-      )
+      ['encodePacked'].includes(node.vFunctionName)
     ) {
       node.vArguments
         .filter((arg) => safeGetNodeType(arg, ast.compilerVersion) instanceof AddressType)
-        .forEach((arg) =>
-          node.vFunctionName === 'encodePacked'
-            ? this.addressesToAbiEncode.push(['a', arg])
-            : this.addressesToAbiEncode.push(['b', arg]),
-        );
+        .forEach((arg) => this.addressesToAbiEncode.push(arg));
     }
+
+    console.log(this.addressesToAbiEncode.length);
     return this.visitExpression(node, ast);
   }
 
   static map(ast: AST): AST {
-    const addresses = new Map<string, [string, ASTNode][]>();
+    const addresses = new Map<string, ASTNode[]>();
 
     ast.roots.forEach((sourceUnit) => {
       const mapper = new this();
       mapper.dispatchVisit(sourceUnit, ast);
       addresses.set(sourceUnit.absolutePath, mapper.addressesToAbiEncode);
     });
+
+    [...addresses.entries()].forEach(([path, nodes]) => {
+      const content = fs.readFileSync(path, { encoding: 'utf-8' });
+      const warnx = [
+        `File ${path}:\n`,
+        ...getSourceFromLocation(content, parseSourceLocation(nodes[0].src))
+          .split('\n')
+          .map((l) => `\t${l}`),
+      ].join('\n');
+
+      console.log(warnx);
+    });
+
     return ast;
   }
 }
 
-function warn(message: string, node: ASTNode) {
-  console.warn(`${warning(message)}${`\n\n${getSourceCode(node)}\n`}`);
+interface Source {
+  source: string;
+  location: SourceLocation;
 }
+function getSourcesFromLocations(sources: Source[]) {}
