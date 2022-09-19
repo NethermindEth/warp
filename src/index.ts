28150c780ca3b713b266ac7aef28d9c9dfae63bf
syahirAmali <syahiramali@gmail.com>
2022-09-19 17:48:54 +0800

    Revert "revert: index"
    This reverts commit a03ee2b53752e409f5ff3a0a2b529cd4e4be7fce.

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,12 @@
 import { Command } from 'commander';
-import {
-  createCompileProgram,
-  createStatusProgram,
-  createDeployProgram,
-  createDeployAccountProgram,
-  createInvokeProgram,
-  createCallProgram,
-  createDeclareProgram,
-  createInstallProgram,
-  createVersionProgram,
-  createAnalyseProgram,
-  createTestProgram,
-  createTransformProgram,
-  createTranspileProgram,
-} from './programFactory';
+import { programs } from './programFactory';
 
 export const program = new Command();
 
 const placeholderValue = { val: '' };
 
-createCompileProgram(program, placeholderValue);
-createStatusProgram(program, placeholderValue);
-createDeployProgram(program, placeholderValue);
-createDeployAccountProgram(program, placeholderValue);
-createInvokeProgram(program, placeholderValue);
-createCallProgram(program, placeholderValue);
-createDeclareProgram(program, placeholderValue);
-createInstallProgram(program);
-createVersionProgram(program);
-createAnalyseProgram(program);
-createTestProgram(program);
-createTransformProgram(program);
-createTranspileProgram(program);
-// for (const prog of programs) {
-//   prog(program, placeholderValue);
-// }
+for (const prog of programs) {
+  prog(program, placeholderValue);
+}
 
 program.parse(process.argv);
