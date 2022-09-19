a03ee2b53752e409f5ff3a0a2b529cd4e4be7fce
syahirAmali <syahiramali@gmail.com>
2022-09-19 16:07:26 +0800

    revert: index

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,39 @@
 import { Command } from 'commander';
-import { programs } from './programFactory';
+import {
+  createCompileProgram,
+  createStatusProgram,
+  createDeployProgram,
+  createDeployAccountProgram,
+  createInvokeProgram,
+  createCallProgram,
+  createDeclareProgram,
+  createInstallProgram,
+  createVersionProgram,
+  createAnalyseProgram,
+  createTestProgram,
+  createTransformProgram,
+  createTranspileProgram,
+} from './programFactory';
 
 export const program = new Command();
 
 const placeholderValue = { val: '' };
 
-for (const prog of programs) {
-  prog(program, placeholderValue);
-}
+createCompileProgram(program, placeholderValue);
+createStatusProgram(program, placeholderValue);
+createDeployProgram(program, placeholderValue);
+createDeployAccountProgram(program, placeholderValue);
+createInvokeProgram(program, placeholderValue);
+createCallProgram(program, placeholderValue);
+createDeclareProgram(program, placeholderValue);
+createInstallProgram(program);
+createVersionProgram(program);
+createAnalyseProgram(program);
+createTestProgram(program);
+createTransformProgram(program);
+createTranspileProgram(program);
+// for (const prog of programs) {
+//   prog(program, placeholderValue);
+// }
 
 program.parse(process.argv);
