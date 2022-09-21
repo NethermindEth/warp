cb83ae6f9ed16357ba4564c7a1e1b86123cead48
Alejandro Labourdette <jal.labourdette@gmail.com>
2022-09-21 16:58:22 -0400

    nonsupport conditionals (for now) v2

--- src/testing.ts
+++ src/testing.ts
@@ -55,6 +55,7 @@ const expectedResults = new Map<string, ResultType>(
     ['example_contracts/c2c', 'Success'],
     // Uses conditionals explicitly
     ['example_contracts/conditional', 'WillNotSupport'],
+    ['example_contracts/conditional_simple', 'WillNotSupport'],
     ['example_contracts/contract_to_contract', 'Success'],
     ['example_contracts/calldatacopy', 'WillNotSupport'],
     ['example_contracts/calldataCrossContractCalls', 'Success'],
