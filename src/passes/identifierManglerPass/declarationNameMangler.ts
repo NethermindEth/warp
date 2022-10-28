67cf2cce191645a1a472f1395b316499768dd518
Carmen Cabrera <kr1000a@gmail.com>
2022-10-28 01:27:02 -0400

    remove counter from generated identifier names

--- src/passes/identifierManglerPass/declarationNameMangler.ts
+++ src/passes/identifierManglerPass/declarationNameMangler.ts
@@ -1,17 +1,17 @@
 import {
-  VariableDeclaration,
-  FunctionDefinition,
-  StructDefinition,
-  SourceUnit,
-  ContractDefinition,
   ASTNode,
+  ContractDefinition,
   EventDefinition,
+  FunctionDefinition,
+  SourceUnit,
+  StructDefinition,
+  VariableDeclaration,
 } from 'solc-typed-ast';
 import { ABIEncoderVersion } from 'solc-typed-ast/dist/types/abi';
 import { AST } from '../../ast/ast';
 import { ASTMapper } from '../../ast/mapper';
 import { printNode } from '../../utils/astPrinter';
-import { WillNotSupportError, TranspileFailedError } from '../../utils/errors';
+import { WillNotSupportError } from '../../utils/errors';
 import { MANGLED_WARP } from '../../utils/nameModifiers';
 import { isNameless } from '../../utils/utils';
 import { safeCanonicalHash } from '../../utils/nodeTypeProcessing';
@@ -83,33 +83,11 @@ export function checkSourceTerms(term: string, node: ASTNode) {
 }
 
 export class DeclarationNameMangler extends ASTMapper {
-  lastUsedId = 0;
-  // Feel free to increase this value. The greater the value the less probably to
-  // execute the code to fix Id's width, but less legible variable names would be.
-  initialIdWidth = 2;
-  nodesNameModified: ASTNode[] = [];
-
   visitSourceUnit(node: SourceUnit, ast: AST): void {
     node.vStructs.forEach((s) => this.mangleStructDefinition(s));
     node.vFunctions.forEach((n) => this.mangleFunctionDefinition(n, ast));
     node.vContracts.forEach((n) => this.mangleContractDefinition(n, ast));
     this.commonVisit(node, ast);
-
-    // Checking if counter is greater than initialIdWidth digits. If so, names are
-    // modified to insert 0's to achieve id's fixed width.
-    const lastIdSize = this.lastUsedId.toString().length;
-    if (lastIdSize > this.initialIdWidth) {
-      this.nodesNameModified.forEach((node) => {
-        if (node instanceof FunctionDefinition) {
-          node.name = this.updateName(node.name, lastIdSize);
-        } else if (node instanceof VariableDeclaration) {
-          node.name = this.updateName(node.name, lastIdSize);
-        } else {
-          throw new TranspileFailedError(`Not expected node to update name: ${node}`);
-        }
-      });
-    }
-    this.nodesNameModified = []; // Lose all references to avoid consuming unnecessary memory
   }
 
   visitStructDefinition(_node: StructDefinition, _ast: AST): void {
@@ -121,9 +99,7 @@ export class DeclarationNameMangler extends ASTMapper {
   visitVariableDeclaration(node: VariableDeclaration, ast: AST): void {
     // state variables should have already been mangled at this point
     // by mangleContractDefinition when visiting SourceUnit nodes
-    if (!node.stateVariable) {
-      this.mangleVariableDeclaration(node);
-    }
+    if (!node.stateVariable) this.mangleVariableDeclaration(node);
 
     this.commonVisit(node, ast);
   }
@@ -133,17 +109,11 @@ export class DeclarationNameMangler extends ASTMapper {
     return !isNameless(fd) ? `${fd.name}_${safeCanonicalHash(fd, ast)}` : fd.name;
   }
 
-  // Return a new id formatted to achieve the minimum length
-  getFormattedId(): string {
-    return (this.lastUsedId++).toString().padStart(this.initialIdWidth, '0');
-  }
-
   createNewVariableName(existingName: string): string {
-    return `${MANGLED_WARP}${this.getFormattedId()}_${existingName}`;
+    return `${MANGLED_WARP}${existingName}`;
   }
 
   mangleVariableDeclaration(node: VariableDeclaration): void {
-    this.nodesNameModified.push(node);
     node.name = this.createNewVariableName(node.name);
   }
 
@@ -166,15 +136,5 @@ export class DeclarationNameMangler extends ASTMapper {
     node.vFunctions.forEach((n) => this.mangleFunctionDefinition(n, ast));
     node.vStateVariables.forEach((v) => this.mangleVariableDeclaration(v));
     node.vEvents.forEach((e) => this.mangleEventDefinition(e));
-  }
-
-  // Set Id of a node name that follows a certain pattern to a fixed width
-  updateName(name: string, lastIdSize: number) {
-    const match = new RegExp(`${MANGLED_WARP}([0-9]+)`).exec(name);
-    if (match === null) {
-      throw new TranspileFailedError(`Expected ${MANGLED_WARP} node name: ${name}`);
-    }
-    const newName = `${MANGLED_WARP}${match[1].padStart(lastIdSize, '0')}`;
-    return name.replace(new RegExp(`${MANGLED_WARP}([0-9]+)`), newName);
   }
 }
