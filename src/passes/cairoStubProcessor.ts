4f63dc152d221b66304569812a8c55b7c724abe7
Rohit Ranjan <rohitrjn629@gmail.com>
2022-11-02 22:08:36 +0530

    Revert changes from cairoStub pass

--- src/passes/cairoStubProcessor.ts
+++ src/passes/cairoStubProcessor.ts
@@ -1,10 +1,5 @@
 import assert from 'assert';
-import {
-  ASTNode,
-  ContractDefinition,
-  FunctionDefinition,
-  StructuredDocumentation,
-} from 'solc-typed-ast';
+import { ASTNode, FunctionDefinition, StructuredDocumentation } from 'solc-typed-ast';
 import { AST } from '../ast/ast';
 import { CairoContract } from '../ast/cairoNodes';
 import { ASTMapper } from '../ast/mapper';
@@ -17,23 +12,6 @@ import { MANGLED_LOCAL_VAR } from '../utils/nameModifiers';
 import { isExternallyVisible } from '../utils/utils';
 
 export class CairoStubProcessor extends ASTMapper {
-  visitContractDefinition(node: ContractDefinition, ast: AST): void {
-    // Only for interaction between plane cairo contract and solidity contract
-    // To support interface call forwarder
-
-    if (!isCairoStub(node)) {
-      this.commonVisit(node, ast);
-      return;
-    }
-
-    let documentation = getDocString(node.documentation);
-    assert(documentation !== undefined);
-    documentation = processDecoratorTags(documentation);
-
-    setDocString(node, documentation);
-    this.commonVisit(node, ast);
-  }
-
   visitFunctionDefinition(node: FunctionDefinition, _ast: AST): void {
     if (!isCairoStub(node)) return;
 
@@ -55,17 +33,13 @@ function processDecoratorTags(documentation: string): string {
 function processStateVarTags(documentation: string, node: FunctionDefinition): string {
   const contract = node.getClosestParentByType(CairoContract);
   const errorNode = node.documentation instanceof ASTNode ? node.documentation : node;
-  const regex = /STATEVAR\((.*?)\)/g;
   if (contract === undefined) {
-    if ([...documentation.matchAll(regex)].length > 0) {
-      throw new WillNotSupportError(
-        `Cairo stub macro 'STATEVAR' is only allowed in member functions`,
-        errorNode,
-      );
-    }
-    return documentation;
+    throw new WillNotSupportError(
+      `Cairo stub macro 'STATEVAR' is only allowed in member functions`,
+      errorNode,
+    );
   }
-  return processMacro(documentation, regex, (arg) => {
+  return processMacro(documentation, /STATEVAR\((.*?)\)/g, (arg) => {
     const stateVarNames = contract.vStateVariables.map((decl) => decl.name);
     const matchingStateVars = stateVarNames.filter((name) => {
       const regex = new RegExp(`${MANGLED_LOCAL_VAR}[0-9]+_`);
@@ -93,17 +67,13 @@ function processStateVarTags(documentation: string, node: FunctionDefinition): s
 function processInternalFunctionTag(documentation: string, node: FunctionDefinition): string {
   const contract = node.getClosestParentByType(CairoContract);
   const errorNode = node.documentation instanceof ASTNode ? node.documentation : node;
-  const regex = /INTERNALFUNC\((.*?)\)/g;
   if (contract === undefined) {
-    if ([...documentation.matchAll(regex)].length > 0) {
-      throw new WillNotSupportError(
-        `Cairo stub macro 'INTERNALFUNC' is only allowed in member function definitions`,
-        errorNode,
-      );
-    }
-    return documentation;
+    throw new WillNotSupportError(
+      `Cairo stub macro 'INTERNALFUNC' is only allowed in member function definitions`,
+      errorNode,
+    );
   }
-  return processMacro(documentation, regex, (arg) => {
+  return processMacro(documentation, /INTERNALFUNC\((.*?)\)/g, (arg) => {
     const funcNames = contract.vFunctions.filter((f) => !isExternallyVisible(f)).map((f) => f.name);
     const matchingFuncs = funcNames.filter((name) => {
       const regex = new RegExp(`_[0-9a-z]+$`);
@@ -128,17 +98,13 @@ function processInternalFunctionTag(documentation: string, node: FunctionDefinit
 function processCurrentFunctionTag(documentation: string, node: FunctionDefinition): string {
   const contract = node.getClosestParentByType(CairoContract);
   const errorNode = node.documentation instanceof ASTNode ? node.documentation : node;
-  const regex = /CURRENTFUNC\((.*?)\)/g;
   if (contract === undefined) {
-    if ([...documentation.matchAll(regex)].length > 0) {
-      throw new WillNotSupportError(
-        `Cairo stub macro 'CURRENTFUNC' is only allowed in member function definitions`,
-        errorNode,
-      );
-    }
-    return documentation;
+    throw new WillNotSupportError(
+      `Cairo stub macro 'CURRENTFUNC' is only allowed in member function definitions`,
+      errorNode,
+    );
   }
-  return processMacro(documentation, regex, (arg) => {
+  return processMacro(documentation, /CURRENTFUNC\((.*?)\)/g, (arg) => {
     if (arg !== '') {
       throw new TranspileFailedError(
         `CURRENTFUNC macro must take no arguments, "${arg}" was provided`,
@@ -161,7 +127,7 @@ function processMacro(
   }, documentation);
 }
 
-function setDocString(node: FunctionDefinition | ContractDefinition, docString: string): void {
+function setDocString(node: FunctionDefinition, docString: string): void {
   const existingDoc = node.documentation;
   if (existingDoc instanceof StructuredDocumentation) {
     existingDoc.text = docString;
@@ -178,7 +144,7 @@ export function getDocString(
   return doc.text;
 }
 
-export function isCairoStub(node: FunctionDefinition | ContractDefinition): boolean {
+export function isCairoStub(node: FunctionDefinition): boolean {
   const documentation = getDocString(node.documentation);
   return documentation !== undefined && documentation.split('\n')[0]?.trim() === 'warp-cairo';
 }
