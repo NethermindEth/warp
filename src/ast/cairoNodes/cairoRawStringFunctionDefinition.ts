7a88b6e3754a9d2646851d31c4bbd3dd33f15513
rodrigo-pino <rodrodpino@gmail.com>
2023-01-25 22:01:28 -0500

    Bug fix to raw string function

--- src/ast/cairoNodes/cairoRawStringFunctionDefinition.ts
+++ src/ast/cairoNodes/cairoRawStringFunctionDefinition.ts
@@ -5,9 +5,7 @@ import {
   ParameterList,
 } from 'solc-typed-ast';
 import { CairoFunctionDefinition, FunctionStubKind } from './cairoFunctionDefinition';
-import { Implicits, implicitTypes } from '../../utils/implicits';
-import { TranspileFailedError } from '../../utils/errors';
-import assert from 'assert';
+import { getRawCairoFunctionInfo } from '../../utils/cairoParsing';
 
 export class CairoRawStringFunctionDefinition extends CairoFunctionDefinition {
   rawStringDefinition: string;
@@ -21,6 +19,7 @@ export class CairoRawStringFunctionDefinition extends CairoFunctionDefinition {
     stateMutability: FunctionStateMutability,
     parameters: ParameterList,
     returnParameters: ParameterList,
+    functionSutbKind: FunctionStubKind,
     rawStringDefinition: string,
   ) {
     super(
@@ -29,51 +28,20 @@ export class CairoRawStringFunctionDefinition extends CairoFunctionDefinition {
       scope,
       kind,
       name,
-      false,
+      false, // Virtual
       visibility,
       stateMutability,
-      false,
+      false, // IsConstructor
       parameters,
       returnParameters,
-      [],
-      new Set(parseImplicits(rawStringDefinition)),
-      FunctionStubKind.None,
-      false,
-      false,
+      [], // Modifier Invocation
+      functionSutbKind === FunctionStubKind.FunctionDefStub
+        ? new Set(getRawCairoFunctionInfo(rawStringDefinition).implicits)
+        : new Set(),
+      functionSutbKind,
+      false, // Accepts raw dynamic array
+      false, // Acceps structured dynamic array
     );
     this.rawStringDefinition = rawStringDefinition;
   }
-}
-function parseImplicits(cairoCode: string): Implicits[] {
-  const funcSignature = cairoCode.match(/func .+\{(.+)\}/);
-  assert(funcSignature !== null);
-
-  // implicits -> impl1 : type1, impl2, ..., impln : typen
-  const implicits = funcSignature[1];
-
-  // implicitsList -> [impl1 : type1, impl2, ...., impln : typen]
-  const implicitsList = [...implicits.matchAll(/[A-Za-z][A-Za-z_: 0-9]*/g)].map((w) => w[0]);
-
-  // implicitsNameList -> [impl1, impl2, ..., impln]
-  const implicitsNameList = implicitsList.map((i) => i.match(/[A-Za-z][A-Za-z_0-9]*/));
-  assert(notContainsNull(implicitsNameList));
-
-  // Check that implicits are valid and add them to result
-  return implicitsNameList.map((i) => {
-    const impl = i[0];
-    if (!elementIsImplicit(impl)) {
-      throw new TranspileFailedError(
-        `Implicit ${impl} defined on raw string function ({printNode(node)}) is not known`,
-      );
-    }
-    return impl;
-  });
-}
-
-function elementIsImplicit(e: string): e is Implicits {
-  return Object.keys(implicitTypes).includes(e);
-}
-
-function notContainsNull<T>(l: (T | null)[]): l is T[] {
-  return !l.some((e) => e === null);
 }
