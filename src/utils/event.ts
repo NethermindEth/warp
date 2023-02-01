29e40b948be2347f66250eb19393bf1b2ada671b
Rohit Ranjan <rohitrjn629@gmail.com>
2023-02-01 11:06:29 +0530

    Remove 251 bit (now it's 256) keccak hashing: Events (#857)
    * keep 256 bits of event topic
    
    * use util fn
    
    * lint
    
    * avoid repetitive hashing
    
    * add behv tests for array of strings: ensure eles are padded
    
    * lint
    
    * remove 251 bits masking
    
    * rmv warp_keccak_felt
    
    * address PR comment
    
    * add pattern matching
    
    * add pattern matching
    
    * address PR comments

--- src/utils/event.ts
+++ src/utils/event.ts
@@ -1,5 +1,5 @@
 import createKeccakHash from 'keccak';
-import { MASK_250 } from '../cairoUtilFuncGen/event';
+import { toUintOrFelt } from './export';
 
 export type EventItem = { data: string[]; keys: string[]; order: number };
 
@@ -48,16 +48,29 @@ export function decodeEventLog(eventsLog: EventItem[]): EventItem[] {
 export type argType = string | argType[];
 
 // NOTE: argTypes must not contain `uint` , it should be `uint256` instead
-export function warpEventCanonicalSignaturehash(eventName: string, argTypes: argType[]): string {
+export function warpEventCanonicalSignaturehash256(
+  eventName: string,
+  argTypes: argType[],
+): { low: string; high: string } {
   const getArgStringRepresentation = (arg: argType): string => {
     if (typeof arg === 'string') return arg;
     return `(${arg.map(getArgStringRepresentation).join(',')})`;
   };
 
   const funcSignature = `${eventName}(${argTypes.map(getArgStringRepresentation).join(',')})`;
-  const funcSignatureHash = createKeccakHash('keccak256').update(funcSignature).digest('hex');
+  return warpEventSignatureHash256FromString(funcSignature);
+}
 
-  return `0x${(
-    BigInt(`0x${createKeccakHash('keccak256').update(funcSignatureHash).digest('hex')}`) & MASK_250
-  ).toString(16)}`;
+// NOTE: argTypes must not contain `uint` , it should be `uint256` instead
+export function warpEventSignatureHash256FromString(functionSignature: string): {
+  low: string;
+  high: string;
+} {
+  const funcSignatureHash = createKeccakHash('keccak256').update(functionSignature).digest('hex');
+  const [low, high] = toUintOrFelt(BigInt(`0x${funcSignatureHash}`), 256);
+
+  return {
+    low: `0x${low.toString(16)}`,
+    high: `0x${high.toString(16)}`,
+  };
 }
