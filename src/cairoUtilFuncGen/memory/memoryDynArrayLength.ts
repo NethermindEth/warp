25c9b81a4c5cfbf9c3e598c1f27d4d0e3ab53740
rodrigo-pino <rodrodpino@gmail.com>
2023-02-03 10:59:13 -0500

    Fix memory dyn array length

--- src/cairoUtilFuncGen/memory/memoryDynArrayLength.ts
+++ src/cairoUtilFuncGen/memory/memoryDynArrayLength.ts
@@ -1,35 +1,21 @@
 import { AST } from '../../ast/ast';
 import { MemberAccess, FunctionCall, DataLocation, generalizeType } from 'solc-typed-ast';
-import { createCairoFunctionStub, createCallToFunction } from '../../utils/functionGeneration';
+import { createCallToFunction } from '../../utils/functionGeneration';
 import { createUint256TypeName } from '../../utils/nodeTemplates';
 import { typeNameFromTypeNode } from '../../utils/utils';
-import { CairoUtilFuncGenBase, StringIndexedFuncGen } from '../base';
 import { safeGetNodeType } from '../../utils/nodeTypeProcessing';
+import { CairoUtilFuncGenBase } from '../base';
 
-export class MemoryDynArrayLengthGen extends StringIndexedFuncGen {
-  getGeneratedCode(): string {
-    return '';
-  }
-
+export class MemoryDynArrayLengthGen extends CairoUtilFuncGenBase {
   gen(node: MemberAccess, ast: AST): FunctionCall {
-    // const arrayType = generalizeType(safeGetNodeType(node.vExpression, ast.inference))[0];
-    // const arrayTypeName = typeNameFromTypeNode(arrayType, ast);
-    // const functionStub = createCairoFunctionStub(
-    //   'wm_dyn_array_length',
-    // TODO: Perhaps this input and output definitions can be used while creating the node
-    //   [['arrayLoc', arrayTypeName, DataLocation.Memory]],
-    //   [['len', createUint256TypeName(this.ast)]],
-    //   ['warp_memory'],
-    //   this.ast,
-    //   node,
-    // );
-    const funcDef = this.getOrCreateFuncGen();
-    const call = createCallToFunction(funcDef, [node.vExpression], this.ast);
-    return call;
-  }
-
-  getOrCreateFuncGen() {
-    const funcDef = this.requireImport('warplib.memory', 'wm_dyn_array_length');
-    return funcDef;
+    const arrayType = generalizeType(safeGetNodeType(node.vExpression, ast.inference))[0];
+    const arrayTypeName = typeNameFromTypeNode(arrayType, ast);
+    const funcDef = this.requireImport(
+      'warplib.memory',
+      'wm_dyn_array_length',
+      [['arrayLoc', arrayTypeName, DataLocation.Memory]],
+      [['len', createUint256TypeName(this.ast)]],
+    );
+    return createCallToFunction(funcDef, [node.vExpression], this.ast);
   }
 }
