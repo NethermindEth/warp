2fe9fc655b8197b477d0cc55a0604391d90b7ef7
Luis Lara <62342483+LuisLara-UH@users.noreply.github.com>
2023-02-13 15:30:07 -0600

    Visited nodes in dfs marked by function name Removing only unreachable functions reachable by contract removed functions

--- src/passes/functionPruner/functionRemover.ts
+++ src/passes/functionPruner/functionRemover.ts
@@ -7,44 +7,61 @@ import { isExternallyVisible } from '../../utils/utils';
 
 export class FunctionRemover extends ASTMapper {
   functionGraph: Map<number, FunctionDefinition[]>;
+  reachableFunctions: Set<string>;
+  reachableFunctionsFromRemovedFunctions: Set<string>;
 
   constructor(graph: Map<number, FunctionDefinition[]>) {
     super();
     this.functionGraph = graph;
+    this.reachableFunctions = new Set();
+    this.reachableFunctionsFromRemovedFunctions = new Set();
   }
 
   visitSourceUnit(node: SourceUnit, ast: AST): void {
-    this.removeUnreachableFunctions(node, ast);
     this.commonVisit(node, ast);
-  }
 
-  visitContractDefinition(node: ContractDefinition, _ast: AST) {
-    this.removeUnreachableFunctions(node, _ast);
-  }
+    this.collectReachableFunctions(node);
+    const utilFunGen = ast.getUtilFuncGen(node);
 
-  removeUnreachableFunctions(node: SourceUnit | ContractDefinition, ast: AST) {
-    const reachableFunctions: Set<number> = new Set();
-    // Collect visible functions and obtain ids of all reachable functions
-    node.vFunctions
-      .filter((func) => isExternallyVisible(func))
-      .forEach((func) => this.dfs(func, reachableFunctions));
     // Remove unreachable functions
     node.vFunctions
-      .filter((func) => !reachableFunctions.has(func.id))
+      .filter(
+        (func) =>
+          this.reachableFunctionsFromRemovedFunctions.has(func.name) &&
+          !this.reachableFunctions.has(func.name),
+      )
       .forEach((func) => {
         node.removeChild(func);
-        const utilFunGen = ast.getUtilFuncGen(node);
         utilFunGen.removeGeneratedFunction(func.name);
       });
   }
 
-  dfs(f: FunctionDefinition, visited: Set<number>): void {
-    visited.add(f.id);
+  visitContractDefinition(node: ContractDefinition, _ast: AST) {
+    this.collectReachableFunctions(node);
+
+    // Remove unreachable functions
+    node.vFunctions
+      .filter((func) => !this.reachableFunctions.has(func.name))
+      .forEach((func) => {
+        node.removeChild(func);
+        this.dfs(func, this.reachableFunctionsFromRemovedFunctions);
+      });
+  }
+
+  collectReachableFunctions(node: SourceUnit | ContractDefinition) {
+    // Collect visible functions and obtain ids of all reachable functions
+    node.vFunctions
+      .filter((func) => isExternallyVisible(func))
+      .forEach((func) => this.dfs(func, this.reachableFunctions));
+  }
+
+  dfs(f: FunctionDefinition, visited: Set<string>): void {
+    visited.add(f.name);
 
     const functions = this.functionGraph.get(f.id);
     assert(functions !== undefined, `Function ${printNode(f)} was not added to the functionGraph`);
     functions.forEach((f) => {
-      if (!visited.has(f.id)) this.dfs(f, visited);
+      if (!visited.has(f.name)) this.dfs(f, visited);
     });
   }
 }
