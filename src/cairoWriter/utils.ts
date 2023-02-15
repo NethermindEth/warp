cc406a845383c7ca7dfa2b72b9d30f9ef955a641
AlejandroLabourdette <jal.labourdette@gmail.com>
2023-02-15 01:01:02 -0500

    update import writer

--- src/cairoWriter/utils.ts
+++ src/cairoWriter/utils.ts
@@ -24,10 +24,12 @@ export function writeImports(imports: Map<string, Set<string>>): string {
     );
   }
   return [...imports.entries()]
-    .map(
-      ([location, importedSymbols]) =>
-        `from ${location} import ${[...importedSymbols.keys()].join(', ')}`,
-    )
+    .map(([location, importedSymbols]) => {
+      const toImport = [...importedSymbols.keys()];
+      const toImportFormatted =
+        toImport.length === 1 ? toImport[0] : '{' + toImport.join(', ') + '}';
+      return `use ${location.replaceAll('.', '::')}::${toImportFormatted};`;
+    })
     .join('\n');
 }
 
