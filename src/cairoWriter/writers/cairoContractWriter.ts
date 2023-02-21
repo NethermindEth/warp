aa62f1b1bb08e69b5f9c7ddd50fa170192938bfe
rodrigo-pino <rodrodpino@gmail.com>
2023-02-21 08:59:21 -0500

    Update writers

--- src/cairoWriter/writers/cairoContractWriter.ts
+++ src/cairoWriter/writers/cairoContractWriter.ts
@@ -1,15 +1,15 @@
-import { ASTWriter, ContractKind, SrcDesc } from 'solc-typed-ast';
+import { ASTWriter, ContractKind, SourceUnit, SrcDesc } from 'solc-typed-ast';
 import { isExternallyVisible } from '../../utils/utils';
-import { CairoContract } from '../../ast/cairoNodes';
+import {
+  CairoContract,
+  CairoGeneratedFunctionDefinition,
+  FunctionStubKind,
+} from '../../ast/cairoNodes';
 import { TEMP_INTERFACE_SUFFIX } from '../../utils/nameModifiers';
 import { CairoASTNodeWriter } from '../base';
-import {
-  getDocumentation,
-  getInterfaceNameForContract,
-  INCLUDE_CAIRO_DUMP_FUNCTIONS,
-  INDENT,
-} from '../utils';
+import { getDocumentation, getInterfaceNameForContract, INDENT } from '../utils';
 import { interfaceNameMappings } from './sourceUnitWriter';
+import assert from 'assert';
 
 export class CairoContractWriter extends CairoASTNodeWriter {
   writeInner(node: CairoContract, writer: ASTWriter): SrcDesc {
@@ -70,12 +70,21 @@ export class CairoContractWriter extends CairoASTNodeWriter {
       .map((l) => (l.length > 0 ? INDENT + l : l))
       .join('\n');
 
+    const parentSourceUnit = node.parent;
+    assert(parentSourceUnit instanceof SourceUnit);
+    const otherStorageVars = parentSourceUnit.vFunctions.filter(
+      (v) =>
+        v instanceof CairoGeneratedFunctionDefinition &&
+        v.functionStubKind === FunctionStubKind.StorageDefStub,
+    );
     const storageCode = [
       'struct Storage {',
       '   WARP_STORAGE: LegacyMap::<felt, felt>,',
       '   WARP_USED_STORAGE: felt,',
       '   WARP_NAMEGEN: felt,',
+      ...otherStorageVars.map((v) => writer.write(v)),
       '}',
+
       'fn readId(loc: felt) -> (val: felt){',
       '    let id = WARP_STORAGE::read(loc);',
       '    if id == 0{',
@@ -87,28 +96,6 @@ export class CairoContractWriter extends CairoASTNodeWriter {
       '        return id;',
       '    }',
       '}',
-      ...(INCLUDE_CAIRO_DUMP_FUNCTIONS
-        ? [
-            'func DUMP_WARP_STORAGE_ITER{syscall_ptr : felt*, pedersen_ptr : HashBuiltin*, range_check_ptr}(length : felt, ptr: felt*){',
-            '    alloc_locals;',
-            '    if (length == 0){',
-            '        return ();',
-            '    }',
-            '    let index = length - 1;',
-            '    let (read) = WARP_STORAGE.read(index);',
-            '    assert ptr[index] = read;',
-            '    DUMP_WARP_STORAGE_ITER(index, ptr);',
-            '    return ();',
-            '}',
-            '@external',
-            'func DUMP_WARP_STORAGE{syscall_ptr : felt*, pedersen_ptr : HashBuiltin*, range_check_ptr}(length : felt) -> (data_len : felt, data: felt*){',
-            '    alloc_locals;',
-            '    let (p: felt*) = alloc();',
-            '    DUMP_WARP_STORAGE_ITER(length, p);',
-            '    return (length, p);',
-            '}',
-          ]
-        : []),
     ].join('\n');
 
     return [
