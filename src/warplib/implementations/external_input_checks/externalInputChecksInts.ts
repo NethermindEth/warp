65a791abc5466d9d91f7ebe05125f0e465ccaa15
AlejandroLabourdette <jal.labourdette@gmail.com>
2023-02-27 11:07:13 -0500

    remove input checks for u256

--- src/warplib/implementations/external_input_checks/externalInputChecksInts.ts
+++ src/warplib/implementations/external_input_checks/externalInputChecksInts.ts
@@ -7,12 +7,9 @@ export function external_input_check_ints(): WarplibFunctionInfo {
     fileName: 'external_input_check_ints',
     imports: [],
     functions: forAllWidths((int_width) => {
+      // These functions will not be needed when we transition to map solidity uintN to cairo uN
       if (int_width === 256) {
-        return [
-          `fn warp_external_input_check_int256(x : u256) {`,
-          `${INDENT}return ();`,
-          `}\n`,
-        ].join('\n');
+        return '';
       } else {
         return [
           `fn warp_external_input_check_int${int_width}(x : felt){`,
