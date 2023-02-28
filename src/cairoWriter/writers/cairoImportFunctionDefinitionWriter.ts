91c15303a6b2baffed5f4ec90c9ecce4ab6f7e86
rodrigo-pino <rodrodpino@gmail.com>
2023-02-28 09:01:37 -0500

    Add comment

--- src/cairoWriter/writers/cairoImportFunctionDefinitionWriter.ts
+++ src/cairoWriter/writers/cairoImportFunctionDefinitionWriter.ts
@@ -2,6 +2,7 @@ import { ASTWriter, SrcDesc } from 'solc-typed-ast';
 import { CairoImportFunctionDefinition } from '../../ast/cairoNodes';
 import { CairoASTNodeWriter } from '../base';
 
+// Not being used as for now
 export class CairoImportFunctionDefinitionWriter extends CairoASTNodeWriter {
   writeInner(node: CairoImportFunctionDefinition, _writer: ASTWriter): SrcDesc {
     return [`from ${node.path} import ${node.name}`];
