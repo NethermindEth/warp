89b466b03a83a434db08c988cce57c0d20cddfbc
AlejandroLabourdette <jal.labourdette@gmail.com>
2023-03-03 10:24:20 -0500

    remove Uint256 warp structure from literal writer

--- src/cairoWriter/writers/literalWriter.ts
+++ src/cairoWriter/writers/literalWriter.ts
@@ -1,22 +1,11 @@
 import { ASTWriter, Literal, LiteralKind, SrcDesc } from 'solc-typed-ast';
-import { TranspileFailedError } from '../../utils/errors';
-import { divmod, primitiveTypeToCairo } from '../../utils/utils';
 import { CairoASTNodeWriter } from '../base';
 
 export class LiteralWriter extends CairoASTNodeWriter {
   writeInner(node: Literal, _: ASTWriter): SrcDesc {
     switch (node.kind) {
       case LiteralKind.Number:
-        switch (primitiveTypeToCairo(node.typeString)) {
-          case 'Uint256': {
-            const [high, low] = divmod(BigInt(node.value), BigInt(Math.pow(2, 128)));
-            return [`Uint256(low=${low}, high=${high})`];
-          }
-          case 'felt':
-            return [node.value];
-          default:
-            throw new TranspileFailedError('Attempted to write unexpected cairo type');
-        }
+        return [node.value];
       case LiteralKind.Bool:
         return [node.value === 'true' ? '1' : '0'];
       case LiteralKind.String:
@@ -31,17 +20,7 @@ export class LiteralWriter extends CairoASTNodeWriter {
         return [`0x${node.hexValue}`];
       }
       case LiteralKind.HexString:
-        switch (primitiveTypeToCairo(node.typeString)) {
-          case 'Uint256': {
-            return [
-              `Uint256(low=0x${node.hexValue.slice(32, 64)}, high=0x${node.hexValue.slice(0, 32)})`,
-            ];
-          }
-          case 'felt':
-            return [`0x${node.hexValue}`];
-          default:
-            throw new TranspileFailedError('Attempted to write unexpected cairo type');
-        }
+        return [`0x${node.hexValue}`];
     }
   }
 }
