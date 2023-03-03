9aa1998c03a53e6a011c756ec5834457aed0c554
AlejandroLabourdette <jal.labourdette@gmail.com>
2023-03-03 10:19:15 -0500

    update assert writer

--- src/cairoWriter/writers/cairoAssertWriter.ts
+++ src/cairoWriter/writers/cairoAssertWriter.ts
@@ -1,20 +1,11 @@
 import { ASTWriter, SrcDesc } from 'solc-typed-ast';
 import { CairoAssert } from '../../ast/cairoNodes';
 import { CairoASTNodeWriter } from '../base';
-import { INDENT } from '../utils';
 
 export class CairoAssertWriter extends CairoASTNodeWriter {
   writeInner(node: CairoAssert, writer: ASTWriter): SrcDesc {
-    const assertExpr = `assert ${writer.write(node.vExpression)} = 1;`;
-
-    if (node.assertMessage === null) {
-      return [assertExpr];
-    } else {
-      return [
-        [`with_attr error_message("${node.assertMessage}"){`, `${INDENT}${assertExpr}`, `}`].join(
-          '\n',
-        ),
-      ];
-    }
+    const expression = writer.write(node.vExpression);
+    const message = node.assertMessage ?? 'Assertion error';
+    return [`assert( ${expression}, "${message}" );`];
   }
 }
