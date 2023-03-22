b8898023162210388e7ddcb6bc8a4d0ef8163240
rodrigo-pino <rodrodpino@gmail.com>
2023-03-22 11:47:08 -0400

    revert last commit

--- src/utils/importFuncGenerator.ts
+++ src/utils/importFuncGenerator.ts
@@ -38,7 +38,6 @@ import {
   UINT256_SUB,
   INTO,
 } from './importPaths';
-import assert from 'assert';
 
 export function createImport(
   path: string[],
@@ -126,7 +125,6 @@ export function createImport(
     case encodePath(U256_FROM_FELTS):
       return createFuncImport();
     default:
-      assert(false);
       throw new TranspileFailedError(`Import ${name} from ${path} is not defined.`);
   }
 }
