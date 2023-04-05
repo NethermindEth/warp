ef3699123032bbe4be5362e3eb19d344b2982bca
Rohit Ranjan <rohitrjn629@gmail.com>
2023-04-05 15:44:13 +0530

    remove unused import

--- src/utils/importPaths.ts
+++ src/utils/importPaths.ts
@@ -106,5 +106,4 @@ export const U32_TO_FELT: [string[], string] = [['integer'], 'u32_to_felt252'];
 
 export const GET_CALLER_ADDRESS: [string[], string] = [['starknet'], 'get_caller_address'];
 export const CONTRACT_ADDRESS: [string[], string] = [['starknet'], 'ContractAddress'];
-export const ADDRESS_INTO_FELT: [string[], string] = [['starknet'], 'ContractAddressIntoFelt252'];
 export const INTO: [string[], string] = [['traits'], 'Into'];
