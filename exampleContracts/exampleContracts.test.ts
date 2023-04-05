14ade94633ea6f891af96c5ba4d0df138924843c
Rohit Ranjan <rohitrjn629@gmail.com>
2023-04-05 19:07:35 +0530

    rework compilation tests

--- exampleContracts/exampleContracts.test.ts
+++ exampleContracts/exampleContracts.test.ts
@@ -8,33 +8,7 @@ const WARP_COMPILATION_TEST_PATH = 'exampleContracts';
 const WARP_TEST_FOLDER = path.join(WARP_TEST, WARP_COMPILATION_TEST_PATH);
 
 const expectedResults = new Map<string, ResultType>(
-  [
-    ['ERC20.sol', 'Success'],
-    ['address/7/padding.sol', 'Success'],
-    ['address/7/primeField.sol', 'Success'],
-    ['address/8/padding.sol', 'Success'],
-    ['address/8/primeField.sol', 'Success'],
-    ['comments.sol', 'Success'],
-    ['enums.sol', 'Success'],
-    ['fileWithMinusSignIncluded-.sol', 'Success'],
-    ['freeFunction.sol', 'Success'],
-    ['idManglingTest9.sol', 'Success'],
-    ['inheritance/simple.sol', 'Success'],
-    ['inheritance/super/base.sol', 'Success'],
-    ['inheritance/super/mid.sol', 'Success'],
-    ['inheritance/variables.sol', 'Success'],
-    ['lib.sol', 'Success'],
-    ['multipleVariables.sol', 'Success'],
-    ['nestedStructs.sol', 'Success'],
-    ['simpleStorageVar.sol', 'Success'],
-    ['stateVariables/enums.sol', 'Success'],
-    ['structs.sol', 'Success'],
-    ['typeConversion/explicitTypeConversion.sol', 'Success'],
-    ['userDefinedFunctionCalls.sol', 'Success'],
-    ['userdefinedidentifier.sol', 'Success'],
-    ['usingFor/private.sol', 'Success'],
-    ['usingFor/simple.sol', 'Success'],
-  ].map(([key, result]) => {
+  [['ERC20.sol', 'Success']].map(([key, result]) => {
     return [path.join(WARP_TEST_FOLDER, key), result] as [string, ResultType];
   }),
 );
