458044698c755cfe5ae21a0926ef7847b4c68e6d
AlejandroLabourdette <jal.labourdette@gmail.com>
2023-04-21 06:53:23 -0400

    `update importFuncGenerator`

--- src/utils/importFuncGenerator.ts
+++ src/utils/importFuncGenerator.ts
@@ -10,22 +10,7 @@ import {
   ParameterInfo,
 } from './functionGeneration';
 import { getContainingSourceUnit } from './utils';
-import {
-  U128_TO_FELT,
-  U256_FROM_FELTS,
-  U128_FROM_FELT,
-  INTO,
-  ARRAY,
-  ARRAY_TRAIT,
-  U32_FROM_FELT,
-  U32_TO_FELT,
-  GET_CALLER_ADDRESS,
-  GET_CONTRACT_ADDRESS,
-  CONTRACT_ADDRESS,
-  MEMORY_TRAIT,
-  WARP_MEMORY,
-  U8_TO_FELT,
-} from './importPaths';
+import * as Paths from './importPaths';
 
 export function createImport(
   path: string[],
@@ -72,26 +57,89 @@ export function createImport(
   if (warplibFunc !== undefined) {
     return createFuncImport(...warplibFunc);
   }
-  if (encodePath([path, name]) === encodePath(U128_FROM_FELT)) {
+
+  const pathForStructImport = [
+    Paths.U8_FROM_FELT,
+    Paths.U16_FROM_FELT,
+    Paths.U24_FROM_FELT,
+    Paths.U32_FROM_FELT,
+    Paths.U40_FROM_FELT,
+    Paths.U48_FROM_FELT,
+    Paths.U56_FROM_FELT,
+    Paths.U64_FROM_FELT,
+    Paths.U72_FROM_FELT,
+    Paths.U80_FROM_FELT,
+    Paths.U88_FROM_FELT,
+    Paths.U96_FROM_FELT,
+    Paths.U104_FROM_FELT,
+    Paths.U112_FROM_FELT,
+    Paths.U120_FROM_FELT,
+    Paths.U128_FROM_FELT,
+    Paths.U136_FROM_FELT,
+    Paths.U144_FROM_FELT,
+    Paths.U152_FROM_FELT,
+    Paths.U160_FROM_FELT,
+    Paths.U168_FROM_FELT,
+    Paths.U176_FROM_FELT,
+    Paths.U184_FROM_FELT,
+    Paths.U192_FROM_FELT,
+    Paths.U200_FROM_FELT,
+    Paths.U208_FROM_FELT,
+    Paths.U216_FROM_FELT,
+    Paths.U224_FROM_FELT,
+    Paths.U232_FROM_FELT,
+    Paths.U240_FROM_FELT,
+    Paths.U248_FROM_FELT,
+  ];
+  if (pathForStructImport.some((i) => encodePath([path, name]) === encodePath(i))) {
     return createStructImport();
   }
-  if (
-    [
-      encodePath(GET_CALLER_ADDRESS),
-      encodePath(GET_CONTRACT_ADDRESS),
-      encodePath(CONTRACT_ADDRESS),
-      encodePath(INTO), // Import libraries from Cairo1
-      encodePath(U8_TO_FELT),
-      encodePath(U128_TO_FELT),
-      encodePath(U256_FROM_FELTS),
-      encodePath(ARRAY),
-      encodePath(ARRAY_TRAIT),
-      encodePath(U32_FROM_FELT),
-      encodePath(U32_TO_FELT),
-      encodePath(WARP_MEMORY),
-      encodePath(MEMORY_TRAIT),
-    ].includes(encodePath([path, name]))
-  ) {
+
+  const pathsForFunctionImport = [
+    Paths.DEPLOY,
+    Paths.EMIT_EVENT,
+    Paths.GET_CALLER_ADDRESS,
+    Paths.GET_CONTRACT_ADDRESS,
+    Paths.INTO,
+    Paths.CONTRACT_ADDRESS,
+    Paths.U8_TO_FELT,
+    Paths.U16_TO_FELT,
+    Paths.U24_TO_FELT,
+    Paths.U32_TO_FELT,
+    Paths.U40_TO_FELT,
+    Paths.U48_TO_FELT,
+    Paths.U56_TO_FELT,
+    Paths.U64_TO_FELT,
+    Paths.U72_TO_FELT,
+    Paths.U80_TO_FELT,
+    Paths.U88_TO_FELT,
+    Paths.U96_TO_FELT,
+    Paths.U104_TO_FELT,
+    Paths.U112_TO_FELT,
+    Paths.U120_TO_FELT,
+    Paths.U128_TO_FELT,
+    Paths.U136_TO_FELT,
+    Paths.U144_TO_FELT,
+    Paths.U152_TO_FELT,
+    Paths.U160_TO_FELT,
+    Paths.U168_TO_FELT,
+    Paths.U176_TO_FELT,
+    Paths.U184_TO_FELT,
+    Paths.U192_TO_FELT,
+    Paths.U200_TO_FELT,
+    Paths.U208_TO_FELT,
+    Paths.U216_TO_FELT,
+    Paths.U224_TO_FELT,
+    Paths.U232_TO_FELT,
+    Paths.U240_TO_FELT,
+    Paths.U248_TO_FELT,
+    Paths.U256_FROM_FELTS,
+    Paths.ARRAY,
+    Paths.ARRAY_TRAIT,
+    Paths.WARP_MEMORY,
+    Paths.MEMORY_TRAIT,
+  ];
+  if (pathsForFunctionImport.some((i) => encodePath([path, name]) === encodePath(i))) {
     return createFuncImport();
   }
 
