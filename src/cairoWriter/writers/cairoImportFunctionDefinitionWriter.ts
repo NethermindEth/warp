790b55dfc441ace992b63fb45fe3c804cbb419a3
rodrigo-pino <rodrodpino@gmail.com>
2023-05-09 14:29:52 -0400

    Fix tsc compile bugs

--- src/cairoWriter/writers/cairoImportFunctionDefinitionWriter.ts
+++ src/cairoWriter/writers/cairoImportFunctionDefinitionWriter.ts
@@ -5,8 +5,8 @@ import { CairoASTNodeWriter } from '../base';
 // Not being used as for now
 export class CairoImportFunctionDefinitionWriter extends CairoASTNodeWriter {
   writeInner(node: CairoImportFunctionDefinition, _writer: ASTWriter): SrcDesc {
-    if (node.path[-1].endsWith('Trait')) {
-      return [`use ${node.path.join('::')}`];
+    if (node.path[node.path.length - 1].endsWith('Trait')) {
+      return [`use ${node.path.join('::')};`];
     }
 
     return [`use ${[...node.path, node.name].join('::')};`];
