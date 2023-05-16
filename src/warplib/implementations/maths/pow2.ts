7473cc18a0680c68ce7fc8328645a8543913df75
AlejandroLabourdette <jal.labourdette@gmail.com>
2023-05-16 17:36:37 -0400

    pow2 return now u128

--- src/warplib/implementations/maths/pow2.ts
+++ src/warplib/implementations/maths/pow2.ts
@@ -4,22 +4,18 @@ import { mapRange } from '../../../export';
 import { msb, WarplibFunctionInfo } from '../../utils';
 
 export function pow2_constants(): WarplibFunctionInfo {
-  const pow2_values = mapRange(256, (n) => n).map((n) => {
+  const pow2_values = mapRange(128, (n) => n).map((n) => {
     return endent`
-      fn pow2_${n}() -> u256 {
-        ${
-          n < 128
-            ? `u256{ low:${msb(n + 1)}_u128, high:0_u128 }`
-            : `u256{ low:0_u128, high:${msb(n - 128 + 1)}_u128 }`
-        }
+      fn pow2_${n}() -> u128 {
+        ${msb(n + 1)}_u128
       }
     `;
   });
   const pow2_dispatcher = endent`
-    fn pow2_n(n: felt252) -> u256 {
+    fn pow2_n(n: felt252) -> u128 {
         if n == 0 {
             return pow2_0();
-        ${mapRange(255, (m) => m + 1)
+        ${mapRange(127, (m) => m + 1)
           .map((n) => {
             return endent`
                 } else if n == ${n} {
@@ -30,7 +26,7 @@ export function pow2_constants(): WarplibFunctionInfo {
         } else {
             panic_with_felt252('Not pow2 stored for index');
         }
-        u256{ low:0_u128, high:0_u128}
+        0_u128
     }
   `;
   return {
