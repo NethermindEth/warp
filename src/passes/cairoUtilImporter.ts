e5d2ee0f490dcbb141e1c3c7c8b0f01a10125f8b
Rodrigo <rodrodpino@gmail.com>
2023-05-25 18:27:42 -0400

    Ensure addition of WarpMemory struct

--- src/passes/cairoUtilImporter.ts
+++ src/passes/cairoUtilImporter.ts
@@ -11,8 +11,16 @@ import {
 } from 'solc-typed-ast';
 import { AST } from '../ast/ast';
 import { ASTMapper } from '../ast/mapper';
+import { CairoFunctionDefinition } from '../export';
 import { createImport } from '../utils/importFuncGenerator';
-import { INTO, U256_FROM_FELTS, U128_FROM_FELT, CONTRACT_ADDRESS } from '../utils/importPaths';
+import {
+  INTO,
+  U256_FROM_FELTS,
+  U128_FROM_FELT,
+  CONTRACT_ADDRESS,
+  WARP_MEMORY,
+  WM_INIT,
+} from '../utils/importPaths';
 import { safeGetNodeType } from '../utils/nodeTypeProcessing';
 import { getContainingSourceUnit, primitiveTypeToCairo } from '../utils/utils';
 
@@ -22,14 +30,25 @@ import { getContainingSourceUnit, primitiveTypeToCairo } from '../utils/utils';
   the warplib maths functions as they are added to the code, but for determining if
   Uint256 needs to be imported, it's easier to do it here
 */
-
 export class CairoUtilImporter extends ASTMapper {
   private dummySourceUnit: SourceUnit | undefined;
 
   // Function to add passes that should have been run before this pass
   addInitialPassPrerequisites(): void {
-    const passKeys: Set<string> = new Set<string>([]);
+    // It requires the AnnotateImplicits (An) because we need to know if any functions
+    // uses WarpMemory
+    // It requires Function Prunner (Fp) for it not to delete any of these newly added
+    // imports
+    const passKeys: Set<string> = new Set<string>(['An', 'Fp']);
     passKeys.forEach((key) => this.addPassPrerequisite(key));
+  }
+
+  visitCairoFunctionDefinition(node: CairoFunctionDefinition, ast: AST): void {
+    if (node.implicits.has('warp_memory')) {
+      createImport(...WARP_MEMORY, this.dummySourceUnit ?? node, ast);
+      createImport(...WM_INIT, this.dummySourceUnit ?? node, ast);
+    }
+    this.commonVisit(node, ast);
   }
 
   visitElementaryTypeName(node: ElementaryTypeName, ast: AST): void {
@@ -43,7 +62,7 @@ export class CairoUtilImporter extends ASTMapper {
 
   visitLiteral(node: Literal, ast: AST): void {
     const type = safeGetNodeType(node, ast.inference);
-    if (type instanceof IntType && type.nBits > 251) {
+    if (type instanceof IntType && type.nBits === 256) {
       createImport(...U256_FROM_FELTS, this.dummySourceUnit ?? node, ast);
     }
 
